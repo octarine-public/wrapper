73e513c3df9563cdf2ee6aa0c801ad06656abf06
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2019-05-29 16:41:17 +0300

    .

--- d.ts/Fusion-Native2.d.ts
+++ d.ts/Fusion-Native2.d.ts
@@ -264,7 +264,7 @@ declare class CUnitOrder {
 	readonly queue: boolean
 	readonly issuer: PlayerOrderIssuer_t
 	readonly position: boolean // returns Vector3 to IOBuffer offset 0 on get
-	readonly unit: C_DOTA_BaseNPC
+	readonly unit?: C_DOTA_BaseNPC
 	readonly target: C_BaseEntity
 	readonly ability: C_DOTABaseAbility
 	readonly show_effects: boolean
