7d194fecf9b09ae86ab9b91533ebad1d5a546510
I_GRIN_I <grigmat78@gmail.com>
2019-08-06 00:44:23 +0300

    TP, Jungle, Smoke

--- src_ts/BeAware/Module/ParticleMapHack/Particle.ts
+++ src_ts/BeAware/Module/ParticleMapHack/Particle.ts
@@ -1,7 +1,7 @@
-import { Vector3, RendererSDK, Entity, Game } from "wrapper/Imports";
+import { Vector3, RendererSDK, Entity, Game, Color } from "wrapper/Imports";
 import { State, DrawRGBA, Size, ComboBox } from "./Menu"
 import { ucFirst } from "../../abstract/Function"
-
+const fullColor = new Color(255,255,255)
 let Particle: Array<[number, BigInt?, Entity?, number?, Vector3?]> = [];
 function ClassChecking(entity: Entity) {
 	return entity.m_pBaseEntity instanceof C_DOTA_BaseNPC_Creep_Lane
@@ -16,7 +16,7 @@ export function ParticleCreate(id: number, handle: BigInt, entity: Entity){
 		return;
 	//antimage_blink_start
 	//Debug.ClassDump(entity)
-	Particle.push([id, handle, entity, Game.RawGameTime + 1]);
+	Particle.push([id, handle, entity, Game.RawGameTime + 2]);
 }
 export function ParticleCreateUpdate(id: number, control_point: number, position: Vector3){
 	if (!State.value)
@@ -25,6 +25,9 @@ export function ParticleCreateUpdate(id: number, control_point: number, position
 		Particle.some(([particle_id, handle, entity, Time]) => {
 			if (particle_id !== id)
 				return false;
+			if(handle === 14221266834388661971n && !position.Equals(new Vector3(1200,1,1200))){
+				SendToConsole('play ui/ping')
+			}
 			Particle.push([particle_id, handle, entity, Time, position])
 			return true
 		})
@@ -47,17 +50,30 @@ export function OnDraw() {
 	Particle.forEach(([particle_id, handle, target, Time, position], i) => {
 		if (position === undefined || Time <= Game.RawGameTime)
 			return Particle.splice(i, 1)
-		let pos_particle = RendererSDK.WorldToScreen(position);
-		if (pos_particle === undefined || target === undefined || target.IsVisible)
+		
+		if (target === undefined){
+			if(handle !== 16169843851719108633n && handle !== 14221266834388661971n)
+				RendererSDK.DrawMiniMapIcon("minimap_creep", 500,position,DrawRGBA.Color)
+			else if(handle === 14221266834388661971n && !position.Equals(new Vector3(1200,1,1200))){
+				RendererSDK.DrawMiniMapIcon("minimap_ping", 700,position,DrawRGBA.Color)
+			}
 			return;
+		}
 		let Name = target.Name;
 		if (Name === undefined)
 			return;
+		// RendererSDK.DrawMiniMapIcon(`minimap_heroicon_${Name}`, 500,position,DrawRGBA.Color)
+		RendererSDK.DrawMiniMapIcon(`minimap_heroicon_${Name}`, 900,position,fullColor)
+		if(target.IsVisible)
+			return
+		let pos_particle = RendererSDK.WorldToScreen(position);
+		if(pos_particle === undefined)
+			return
 		switch (ComboBox.selected_id) {
 			case 0: 
 				Renderer.Image(`panorama/images/heroes/icons/${target.Name}_png.vtex_c`, 
 					pos_particle.x - Size.value / 4, pos_particle.y - Size.value / 4, Size.value / 2, Size.value / 2);
-			 break;
+			break;
 			case 1:
 				let NameRenderUnit = Name.split("_").splice(3, 3).join(' ');
 				Renderer.Text(pos_particle.x, pos_particle.y, ucFirst(NameRenderUnit),
