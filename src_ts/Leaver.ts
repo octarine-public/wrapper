ad4d0f627f1d6b61d8c2ed85bee8d44f7634fe10
SicdeX- <sicdex@gmail.com>
2019-08-10 12:53:52 +0600

    fix leaver Player ID, and repeat gap 75

--- src_ts/Leaver.ts
+++ src_ts/Leaver.ts
@@ -9,7 +9,7 @@ const { MenuFactory } = MenuManager,
     leaveFriend = tree.AddKeybind('Leave friend button'),
     pid = tree.AddComboBox('Player ID',['0','1','2','3','4','5','6','7','8','9']),
     colors = ['#415fff','#83ffda','#c3009c','#d5ff16','#f16900','#ff6ca5','#85c83b','#74d6f9','#009e31','#8f6f00'],
-    gap = "<br>".repeat(100),
+    gap = "<br>".repeat(75),
     heroes = {
         "npc_dota_hero_queenofpain":	"Queen of Pain",
         "npc_dota_hero_antimage":	"Anti-Mage",
@@ -128,19 +128,25 @@ const { MenuFactory } = MenuManager,
         "npc_dota_hero_lycan":	"Lycan",
     }
 leave.OnPressed(btn => {
-    const player = PlayerResource.GetPlayerByPlayerID(pid.selected_id),
-        id = player.PlayerID,
-        name = player.Name,
-        hero = player.Hero.Name
+    const player = PlayerResource.GetPlayerByPlayerID(pid.selected_id);
+    if (player === undefined)
+        return;
+    const id = player.PlayerID,
+          name = player.Name, 
+          hero = player.Hero.Name
     ChatWheelAbuse(`${gap}<font color="${colors[id]}">${name} (${heroes[hero]})</font> отключается от игры. Пожалуйста, дождитесь повторного подключения.<br><font color='#FF0000'><b>У <font color="${colors[id]}">${name} (${heroes[hero]})</font> осталось 5 мин. для повторного подключения.</b></font><br><font color="${colors[id]}">${name} (${heroes[hero]})</font> покидает игру.<br><font color='#00FF00'><b>Теперь эту игру можно спокойно покинуть.</b></font>`)
     if(autodisconnect.value) {
         setTimeout(() =>  SendToConsole('disconnect'), Additionaldelay.value*1000)
     }
 });
 leaveFriend.OnPressed(btn => {
-    const player = PlayerResource.GetPlayerByPlayerID(pid.selected_id),
-        id = player.PlayerID,
+    const player = PlayerResource.GetPlayerByPlayerID(pid.selected_id);
+    if (player === undefined)
+        return;
+    const id = player.PlayerID,
         name = player.Name,
-        hero = player.Hero.Name
+        hero = player.Hero.Name;
+        
+
     ChatWheelAbuse(`${gap}<font color="${colors[id]}">${name} (${heroes[hero]})</font> покидает игру.<br><font color='#00FF00'><b>Теперь эту игру можно спокойно покинуть.</b></font>`)
 });
