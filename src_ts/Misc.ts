bac1bfaf812f3a15c0eeb2b50e09585e50433223
SicdeX- <sicdex@gmail.com>
2019-09-02 14:14:12 +0600

    add Mouse wheel and small improvements

--- src_ts/Misc.ts
+++ src_ts/Misc.ts
@@ -1,28 +1,43 @@
 import { EventsSDK, Game, Menu as MenuSDK } from "./wrapper/Imports"
-
+function Language(ru: string, en: string) {
+	return Game.Language === "russian" ? ru : en;
+}
 let Menu = MenuSDK.AddEntry("Misc")
-Menu.AddButton("Reload").OnValue(() => {
+
+let CameraTree = Menu.AddNode(Language("Камера", "Camera")),
+	CamDist = CameraTree.AddSliderFloat(Language("Дистация камеры", "Camera Distance") , 1300, 0, 10000).OnValue(caller => {
+		ConVars.Set("r_farz", caller.value * 2)
+		if (Game.IsInGame)
+			Camera.Distance = caller.value
+	}),
+	CamMouseTree = CameraTree.AddNode(Language("Колёсико мыши", "Mouse wheel")),
+	CamMouseState = CamMouseTree.AddToggle(Language("Включить", "Active")),
+	CamStep = CamMouseTree.AddSliderFloat(Language("Шаг камеры", "Camera Step"), 50, 10, 1000),
+	Weather = Menu.AddSwitcher(Language("Погода", "Weather"), [
+		"Default",
+		"Snow",
+		"Rain",
+		"Moonbeam",
+		"Pestilence",
+		"Harvest",
+		"Sirocco",
+		"Spring",
+		"Ash",
+		"Aurora",
+	], 8).OnValue(caller => ConVars.Set("cl_weather", caller.selected_id));
+	
+Menu.AddKeybind(Language("Меню (Открыть / Закрыть)", "Menu (Open / Close)"), "Insert").OnPressed(() => MenuSDK.MenuManager.is_open = !MenuSDK.MenuManager.is_open).activates_in_menu = true
+
+Menu.AddButton(Language("Перезагрузить скрипты", "Reload Scripts")).OnValue(() => {
 	EventsSDK.emit("GameEnded", false)
 	global.reload("eTE9Te5rgBYThsO", true)
 })
-let CamDist = Menu.AddSliderFloat("Camera Distance", 1300, 0, 10000).OnValue(caller => {
-	ConVars.Set("r_farz", caller.value * 2)
-	if (Game.IsInGame)
-		Camera.Distance = caller.value
+
+CameraTree.AddButton(Language("Сбросить камеру", "Reset camera")).OnValue(() => {
+	Camera.Distance = CamDist.value = 1134
+	ConVars.Set("r_farz", -1)
 })
-let Weather = Menu.AddSwitcher("Weather", [
-	"None",
-	"Snow",
-	"Rain",
-	"Moonbeam",
-	"Pestilence",
-	"Harvest",
-	"Sirocco",
-	"Spring",
-	"Ash",
-	"Aurora",
-], 8).OnValue(caller => ConVars.Set("cl_weather", caller.selected_id))
-Menu.AddKeybind("Menu hotkey", "Insert").OnPressed(() => MenuSDK.MenuManager.is_open = !MenuSDK.MenuManager.is_open).activates_in_menu = true
+
 
 EventsSDK.on("GameConnected", () => {
 	// if (!enable_modifications)
@@ -35,4 +50,22 @@ EventsSDK.on("GameConnected", () => {
 	ConVars.Set("dota_use_particle_fow", false)
 	ConVars.Set("demo_recordcommands", false)
 	ConVars.Set("dota_unit_orders_rate", 512)
+})
+
+EventsSDK.on("WndProc", (msg, wParam) => {
+	if (CamMouseState.value){
+		if (wParam === 7864320n) {	
+			console.log(CamDist.value)
+			let cam = Camera.Distance = CamDist.value -= CamStep.value
+			ConVars.Set("r_farz", cam * 2)
+			return true
+		}
+		if (wParam === 4287102976n) {
+			console.log(CamDist.value)
+			let cam = Camera.Distance = CamDist.value += CamStep.value
+			ConVars.Set("r_farz", cam * 2)
+			return true
+		}
+		return true
+	}
 })
