93c61d2a07c44a8c8735ef7ab3742902a2a14d0d
SicdeX- <sicdex@gmail.com>
2019-10-27 00:49:35 +0600

    Improved Kunkka combo

--- src_ts/ComboHeroes/Kunkka/Module/Combo.ts
+++ src_ts/ComboHeroes/Kunkka/Module/Combo.ts
@@ -18,10 +18,11 @@ let Sleep: GameSleeper = new GameSleeper,
 	AutoCombo: boolean = false,
 	DisableStaticTime = 1.6
 
-function CheckAbility(ability: Ability | Item, target: Hero): boolean {
+function CheckAbility(ability: Ability | Item, target: Hero | Vector3): boolean {
 	return ability !== undefined 
+		&& ability.IsReady && !ability.IsInAbilityPhase
 		&& ability.CanBeCasted() && СomboAbility.IsEnabled(ability.Name) 
-		&& Owner.Distance2D(target) <= ability.CastRange
+		&& Owner.Distance2D(target) <= ability.CastRange && !Sleep.Sleeping(ability)
 }
 
 function XMode(abils: Ability, target: Hero, Time: number, Combo: boolean) {
@@ -34,18 +35,88 @@ function XMode(abils: Ability, target: Hero, Time: number, Combo: boolean) {
 		ComboTimer = Time + 3.08
 	}
 }
+function CastDelay() {
+	return 250
+}
+function ComboInit() {
+	let Items = new InitItems(Owner),
+		Abilities = new InitAbility(Owner),
+		Time = Game.RawGameTime
+	let Q = Abilities.Torrent as Ability,
+		X = Abilities.MarksSpot as Ability,
+		R = Abilities.Ghostship as Ability,
+		RX = Abilities.Return as Ability,
+		RF = Items.Refresher as Item,
+		RFS = Items.RefresherShard as Item,
+		SHG = Items.Shivas as Item
+	if (ShipCombo) {
+		if (XMarkPos.LengthSqr === 0 || X.CanBeCasted()) {
+			return false
+		}
+		if (CheckAbility(R, XMarkPos)) {
+			R.UseAbility(XMarkPos)
+			ComboTimer = Time + 3.08
+			Sleep.Sleep(((R.CastPoint * 2) + CastDelay()), R)
+		}
+	}
+	if (ComboTimer - Time <= 2.05) {
+		if (XMarkPos.LengthSqr === 0 || X.CanBeCasted()) {
+			return false
+		}
+		if (CheckAbility(Q, XMarkPos)) {
+			Q.UseAbility(XMarkPos)
+			Sleep.Sleep(((Q.CastPoint * 2) + CastDelay()), Q)
+		}
+	}
+	if (ComboTimer - Time <= 0.55) {
+		if (SHG !== undefined && SHG.CanBeCasted() && СomboItems.IsEnabled(SHG.Name)) {
+			Owner.CastNoTarget(SHG)
+			Sleep.Sleep(Abilities.Tick + CastDelay(), SHG)
+		}
+		if (RFS !== undefined && RFS.CanBeCasted() && СomboItems.IsEnabled(RFS.Name)) {
+			Owner.CastNoTarget(RFS)
+			Sleep.Sleep(Abilities.Tick + CastDelay(), RFS)
+		}
+		if (RF !== undefined && RF.CanBeCasted() && СomboItems.IsEnabled(RF.Name)) {
+			Owner.CastNoTarget(RF)
+			Sleep.Sleep(Items.Tick + CastDelay(), RF)
+		}
+		if (RX !== undefined && !RX.IsHidden) {
+			Owner.CastNoTarget(RX)
+			Sleep.Sleep(((RX.CastPoint * 2) + CastDelay()), RX)
+		}
+	}
+	if (ComboTimer - Time <= 0.1) {
+		XMarkPos = new Vector3
+		ShipCombo = false
+		AutoCombo = false
+		Sleep.FullReset()
+		return true
+	}
+}
 
 export function InitCombo() {
 	if (!Base.IsRestrictions(State)) {
 		return false
 	}
-	if (Sleep.Sleeping("Delay")) {
-		return false
-	}
 	let target = MouseTarget,
 		Time = Game.RawGameTime
+			
+	let Items = new InitItems(Owner),
+		Abilities = new InitAbility(Owner)
+	let Q = Abilities.Torrent as Ability,
+		X = Abilities.MarksSpot as Ability,
+		R = Abilities.Ghostship as Ability,
+		HEX = Items.Sheeps as Item	
+	if (XMarkPos.LengthSqr !== 0) {
+		ComboInit()
+		return false
+	}
 	if (Base.CanCastSpells(Owner) || target === undefined || target.IsMagicImmune) {
 		return false
+	}
+	if (target !== undefined && XMarkPos.LengthSqr !== 0) {
+		XMarkPos = undefined
 	}
 	let IsStunned = target.GetBuffByName("modifier_bashed"),
 		IsBashed  = target.GetBuffByName("modifier_stunned"),
@@ -59,18 +130,6 @@ export function InitCombo() {
 			AutoCombo = true
 		}
 	}
-	let Items = new InitItems(Owner),
-		Abilities = new InitAbility(Owner)
-		
-	let Q = Abilities.Torrent as Ability,
-		X = Abilities.MarksSpot as Ability,
-		R = Abilities.Ghostship as Ability,
-		RX = Abilities.Return as Ability,
-		RF = Items.Refresher as Item,
-		RFS = Items.RefresherShard as Item,
-		SHG = Items.Shivas as Item,
-		HEX = Items.Sheeps as Item
-		
 	if (ComboTimer < Time) {
 		if (BladeMailItem.value && (BladeMailItem.value && target.HasModifier("modifier_item_blade_mail_reflect")) || !Base.Cancel(target)) {
 			return false
@@ -78,30 +137,36 @@ export function InitCombo() {
 		if (!target.HasModifier("modifier_kunkka_x_marks_the_spot")) {
 			if (ComboKeyItem.is_pressed || AutoCombo) {
 				if (Items.Blink !== undefined
+					&& !Sleep.Sleeping(Items.Blink)
 					&& СomboItems.IsEnabled(Items.Blink.Name)
 					&& !target.IsInRange(Owner, 600)
 					&& Items.Blink.CanBeCasted()
 				) {
 					let castRange = Items.Blink.GetSpecialValue("blink_range") + Owner.CastRangeBonus
 					Items.Blink.UseAbility(Owner.NetworkPosition.Extend(target.NetworkPosition, Math.min(castRange, Owner.Distance(target) - BlinkRadius.value) - 1))
-					Sleep.Sleep(Items.Tick, "Delay")
+					Sleep.Sleep((Items.Tick + CastDelay()), Items.Blink)
 					return false
 				}
 				if (HEX !== undefined 
+					&& !Sleep.Sleeping(HEX)
 					&& HEX.CanBeCasted() && СomboItems.IsEnabled(HEX.Name)
 					&& (hexDebuff === undefined || !hexDebuff.IsValid || hexDebuff.RemainingTime <= 0.3)) {
 					HEX.UseAbility(target)
-					Sleep.Sleep(Abilities.Tick, "Delay")
+					Sleep.Sleep((Items.Tick + CastDelay()), HEX)
 				}
-				if (CheckAbility(X, target) && Q !== undefined && Q.CanBeCasted() || R !== undefined && R.CanBeCasted()) {
+				if (CheckAbility(X, target) 
+					&& Q !== undefined && Q.CanBeCasted() 
+					|| R !== undefined && R.CanBeCasted()) {
 					ShipCombo = true
 					let Predict = target.InFront(600 / 1000 * (target.IsMoving ? target.IdealSpeed : 0))
 					XMarkPos = Predict
 					X.UseAbility(target)
 					XMarkCastTime = Time + 1
-					Sleep.Sleep(Abilities.Tick, "Delay")
+					Sleep.Sleep((X.CastPoint * 2), X)
 				}
-				if (R === undefined || !R.CanBeCasted() || Owner.Distance2D(target) >= R.CastRange) {
+				if (R === undefined || !R.CanBeCasted() 
+					|| Owner.Distance2D(target) >= R.CastRange
+				) {
 					ShipCombo = false
 					ComboTimer = Time + 3.08
 				}
@@ -110,60 +175,30 @@ export function InitCombo() {
 			if (ComboKeyItem.is_pressed || AutoCombo) {
 				if (Q !== undefined && Q.CanBeCasted() || R !== undefined && R.CanBeCasted()) {
 					XMode(X, target, Time, true)
-					Sleep.Sleep(Abilities.Tick, "Delay")
+					Sleep.Sleep((X.CastPoint * 2), X)
 				}
 				return false
 			} else if (ComboKeyTorrent.is_pressed) {
 				if (Q !== undefined && Q.CanBeCasted() || R !== undefined && R.CanBeCasted()) {
 					XMode(X, target, Time, false)
-					Sleep.Sleep(Abilities.Tick, "Delay")
+					Sleep.Sleep((X.CastPoint * 2), X)
 				}
 			}
 		}
-		else {	
-			if (ShipCombo) {
-				if (CheckAbility(R, target)){
-					R.UseAbility(XMarkPos)
-					ComboTimer = Time + 3.08
-					Sleep.Sleep(Abilities.Tick, "Delay")
-				}
+		else {
+			if (XMarkPos.LengthSqr === 0 || X.CanBeCasted()) {
+				return false
 			}
+			ComboInit()
 			return false
 		}
 	}
 	else{
-		if (XMarkPos.LengthSqr === 0) {
+		if (XMarkPos.LengthSqr === 0 || X.CanBeCasted()) {
 			return false
 		}
-		if (ComboTimer - Time <= 2.05) {
-			if (CheckAbility(Q, target)) {
-				Q.UseAbility(XMarkPos)
-				Sleep.Sleep(Abilities.Tick, "Delay")
-			}
-		}
-		if (ComboTimer - Time <= 0.55) {
-			if (SHG !== undefined && SHG.CanBeCasted() && СomboItems.IsEnabled(SHG.Name)) {
-				Owner.CastNoTarget(SHG)
-				Sleep.Sleep(Abilities.Tick, "Delay")
-			}
-			if (RFS !== undefined && RFS.CanBeCasted() && СomboItems.IsEnabled(RFS.Name)) {
-				Owner.CastNoTarget(RFS)
-				Sleep.Sleep(Abilities.Tick, "Delay")
-			}
-			if (RF !== undefined && RF.CanBeCasted() && СomboItems.IsEnabled(RF.Name)) {
-				Owner.CastNoTarget(RF)
-				Sleep.Sleep(Abilities.Tick, "Delay")
-			}
-			if (RX !== undefined && !RX.IsHidden) {
-				Owner.CastNoTarget(RX)
-				Sleep.Sleep(Abilities.Tick, "Delay")
-			}
-		}
-		if (ComboTimer - Time <= 0.1) {
-			XMarkPos = new Vector3
-			ShipCombo = false
-			AutoCombo = false
-		}
+		ComboInit()
+		return false
 	}
 }
 
