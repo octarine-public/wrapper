aad2e8ba111c62c4e3dc94c9a8ff15e02aca6139
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2019-11-02 03:12:52 +0300

    Improved look&feel on replay

--- src_ts/Misc.ts
+++ src_ts/Misc.ts
@@ -1,4 +1,4 @@
-import { EventsSDK, Game, Menu as MenuSDK, RendererSDK, ExecuteOrder, Vector3, Entity, Vector2 } from "./wrapper/Imports"
+import { EventsSDK, Game, Menu as MenuSDK, RendererSDK, ExecuteOrder, Vector3, Entity, Vector2, EntityManager } from "./wrapper/Imports"
 import UserCmd from "./wrapper/Native/UserCmd"
 
 let Menu = MenuSDK.AddEntry("Misc"),
@@ -110,7 +110,7 @@ Events.on("SharedObjectChanged", (id, reason, uuid, obj) => {
 
 let PrepareUnitOrders_old = PrepareUnitOrders
 let last_order_click = new Vector3(),
-	last_order_click_updates = 0
+	last_order_click_update = 0
 global.PrepareUnitOrders = function(order: { // pass Position: Vector3 at IOBuffer offset 0
 	OrderType: dotaunitorder_t,
 	Target?: C_BaseEntity | number,
@@ -129,15 +129,15 @@ global.PrepareUnitOrders = function(order: { // pass Position: Vector3 at IOBuff
 		case dotaunitorder_t.DOTA_UNIT_ORDER_RADAR:
 		case dotaunitorder_t.DOTA_UNIT_ORDER_VECTOR_TARGET_POSITION:
 			last_order_click.CopyFrom(Vector3.fromIOBuffer())
-			last_order_click_updates = 0
+			last_order_click_update = Date.now()
 			break
 		case dotaunitorder_t.DOTA_UNIT_ORDER_ATTACK_TARGET:
 		case dotaunitorder_t.DOTA_UNIT_ORDER_CAST_TARGET:
 		case dotaunitorder_t.DOTA_UNIT_ORDER_MOVE_TO_TARGET:
 		case dotaunitorder_t.DOTA_UNIT_ORDER_PICKUP_ITEM:
 		case dotaunitorder_t.DOTA_UNIT_ORDER_PICKUP_RUNE:
-			last_order_click.CopyFrom(order.Target instanceof Entity ? order.Target.Position : Vector3.fromIOBuffer())
-			last_order_click_updates = 0
+			last_order_click.CopyFrom(order.Target instanceof C_BaseEntity ? EntityManager.GetEntityByNative(order.Target).Position : Vector3.fromIOBuffer())
+			last_order_click_update = Date.now()
 			break
 		default:
 			break
@@ -145,25 +145,39 @@ global.PrepareUnitOrders = function(order: { // pass Position: Vector3 at IOBuff
 	PrepareUnitOrders_old(order)
 }
 
-let experiment = true
+let experiment = true,
+	latest_camera_x = 0,
+	latest_camera_y = 0// ,
+	// latest_mouse_vec = new Vector3()
 EventsSDK.after("Update", (cmd: UserCmd) => {
 	let CursorWorldVec = cmd.VectorUnderCursor,
-		orig_CursorWorldVec = cmd.VectorUnderCursor.Clone()
-	if (last_order_click_updates < 6 && last_order_click.Distance2D(cmd.VectorUnderCursor) > 200) {
-		CursorWorldVec = last_order_click.Extend(CursorWorldVec, 200).CopyTo(last_order_click)
-		last_order_click_updates++
-	} else
-		CursorWorldVec.CopyTo(last_order_click)
-	cmd.VectorUnderCursor = CursorWorldVec.SetZ(RendererSDK.GetPositionHeight(CursorWorldVec.toVector2()))
-	if (!experiment || new Vector3(cmd.CameraX, cmd.CameraY).Distance2D(CursorWorldVec) > 1300 * 1.2) {
-		cmd.CameraX = CursorWorldVec.x - 200
-		cmd.CameraY = CursorWorldVec.y - 1134 / 2 - 200
-		cmd.MouseX = cmd.MouseY = 0.5
-	} else if (orig_CursorWorldVec.Distance(CursorWorldVec) > 100) {
-		let mouse_vec = new Vector2(cmd.MouseX * 100, cmd.MouseY * 100)
-		mouse_vec.x += (CursorWorldVec.x - orig_CursorWorldVec.x) / 10
-		mouse_vec.y += (CursorWorldVec.y - orig_CursorWorldVec.y) / 10
-		cmd.MouseX = Math.min(1, mouse_vec.x / 100)
-		cmd.MouseY = Math.min(1, mouse_vec.y / 100)
+		orig_CursorWorldVec = cmd.VectorUnderCursor.Clone(),
+		mult = Math.sin(Date.now() - last_order_click_update)
+	if (last_order_click_update + 450 >= Date.now()) {
+		CursorWorldVec = last_order_click.Extend(CursorWorldVec, Math.min(last_order_click.Distance(CursorWorldVec), 200 * mult)).CopyTo(last_order_click)
+		cmd.CameraX = latest_camera_x
+		cmd.CameraY = latest_camera_y
 	}
+	cmd.VectorUnderCursor = CursorWorldVec.SetZ(RendererSDK.GetPositionHeight(CursorWorldVec.toVector2()))
+	let camera_vec = new Vector3(cmd.CameraX, cmd.CameraY)
+	if (!experiment || camera_vec.Distance2D(CursorWorldVec) > 1300 * 1.2) {
+		latest_camera_x = cmd.CameraX = CursorWorldVec.x // - 200
+		latest_camera_y = cmd.CameraY = CursorWorldVec.y - 1134 / 2 // - 200
+		cmd.MouseX = cmd.MouseY = 0.5
+		return
+	} else if (orig_CursorWorldVec.Distance(CursorWorldVec) > 100) {
+		let mouse_vec = new Vector2(cmd.MouseX, cmd.MouseY)
+		mouse_vec.x += (CursorWorldVec.x - orig_CursorWorldVec.x) / 1134
+		mouse_vec.y += (CursorWorldVec.y - orig_CursorWorldVec.y) / 1134
+		mouse_vec.x = Math.min(1, mouse_vec.x / 100)
+		mouse_vec.y = Math.min(1, mouse_vec.y / 100)
+		if (mouse_vec.x > 0.1 && mouse_vec.y > 0) {
+			cmd.MouseX = mouse_vec.x
+			cmd.MouseY = mouse_vec.y
+		} else
+			cmd.MouseX = cmd.MouseY = 0.5
+	}
+	camera_vec = camera_vec.AddScalarY(1134 / 2).Extend(CursorWorldVec, Math.min(camera_vec.Distance(CursorWorldVec), 150 * (last_order_click_update + 450 >= Date.now() ? mult : 1))).SubtractScalarY(1134 / 2)
+	latest_camera_x = cmd.CameraX = camera_vec.x
+	latest_camera_y = cmd.CameraY = camera_vec.y
 })
