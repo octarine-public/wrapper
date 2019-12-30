a8a74e68b3061ea544b9f4b5347280a811e4d683
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2019-12-30 10:24:43 +0300

    Fixed IsVisibleForEnemies problem after changing LocalPlayer#Team

--- src_ts/wrapper/Managers/EventsSDK.ts
+++ src_ts/wrapper/Managers/EventsSDK.ts
@@ -336,6 +336,13 @@ Events.on("NetworkFieldsChanged", map => {
 					case "m_iTeamNum":
 						entity.Team = entity.m_pBaseEntity.m_iTeamNum
 						EventsSDK.emit("EntityTeamChanged", false, entity)
+						if (LocalPlayer === entity)
+							EntityManager.GetEntitiesByClass(Unit).forEach(unit => {
+								let old_visibility = unit.IsVisibleForEnemies
+								unit.IsVisibleForEnemies = Unit.IsVisibleForEnemies(unit)
+								if (unit.IsVisibleForEnemies !== old_visibility)
+									EventsSDK.emit("EntityTeamChanged", false, unit)
+							})
 						break
 					case "m_lifeState":
 						entity.LifeState = entity.m_pBaseEntity.m_lifeState
