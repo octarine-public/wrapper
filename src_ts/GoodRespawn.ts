e6cae8927b1776c49ec47cd0c9e7340e4f992628
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2020-01-12 17:00:59 +0300

    Simplified & improved Good Respawn logic

--- src_ts/GoodRespawn.ts
+++ src_ts/GoodRespawn.ts
@@ -1,12 +1,12 @@
-import { Vector3, Team, Hero, pudge_meat_hook, Obstacle, Vector2, Entity, Unit, MovingObstacle, NavMeshPathfinding, MathSDK, Creep, GameSleeper } from "./wrapper/Imports"
+import { Vector3, Team, Hero, pudge_meat_hook, Obstacle, Vector2, Entity, Unit, MovingObstacle, NavMeshPathfinding, Creep, GameSleeper } from "./wrapper/Imports"
 
 let menu = Menu.AddEntry(["Utility", "Good Respawn"])
-let visuals_state = menu.AddToggle("Visuals State", true)
-let debug_visuals_state = menu.AddToggle("Debug Visuals State", false)
-let autohook_state = menu.AddToggle("AutoHook State", true)
-let autohook_delay = menu.AddSlider("AutoHook Delay", 0, -6, 6)
-let lock_position = menu.AddToggle("Lock position to 1 hook", false)
-let manual_fix = menu.AddSlider("Manual position fix", 0, 0, 14)
+let visuals_state = menu.AddToggle("Visuals State", true),
+	debug_visuals_state = menu.AddToggle("Debug Visuals State", false),
+	autohook_state = menu.AddToggle("AutoHook State", true),
+	autohook_delay = menu.AddSlider("AutoHook Delay", 0, -6, 6),
+	lock_position = menu.AddToggle("Lock position to 1 hook", false),
+	manual_fix = menu.AddSlider("Manual position fix", 0, 0, 14)
 
 function GetPositions(): Vector3[] {
 	return EntityManager.AllEntities
@@ -56,51 +56,6 @@ EventsSDK.on("Draw", () => {
 		}
 	})
 })
-
-function TryPredict(
-	start_pos: Vector2,
-	hook: pudge_meat_hook,
-	rad: number,
-	obstacles: Obstacle[],
-	obs2ent: Map<Obstacle, Entity>,
-	owner: Unit,
-	target: Entity,
-): Nullable<[Vector3, number]> {
-	let angle = Vector3.FromAngle(rad)
-	let predict_res = new NavMeshPathfinding(
-		new MovingObstacle(
-			start_pos/*.Add(angle.toVector2().MultiplyScalar(hook.AOERadius * 1.5))*/,
-			hook.AOERadius,
-			angle.toVector2().MultiplyScalarForThis(hook.Speed),
-			hook.CastRange / hook.Speed
-		),
-		obstacles,
-		hook.CastPoint + owner!.TurnTime(owner!.Position.Rotation(angle, 100)) + (autohook_delay.value / 60),
-	).GetFirstHitObstacle()
-	if (predict_res === undefined)
-		return undefined
-	if (obs2ent.get(predict_res[0]) === target)
-		return [angle, predict_res[1]]
-	return undefined
-}
-
-function TryPredictInAngles(
-	base_ang: number,
-	min: number, max: number,
-	start_pos: Vector2,
-	hook: pudge_meat_hook,
-	obstacles: Obstacle[],
-	obs2ent: Map<Obstacle, Entity>,
-	owner: Unit,
-	target: Entity,
-): Nullable<[Vector3, number]> {
-	for (let deg = min; deg < max; deg++) {
-		let predicted_angle = TryPredict(start_pos, hook, base_ang + MathSDK.DegreesToRadian(deg), obstacles, obs2ent, owner, target)
-		if (predicted_angle !== undefined)
-			return predicted_angle
-	}
-	return undefined
-}
 
 function GetEnemyDeaths() {
 	let deaths = 0
@@ -156,25 +111,33 @@ EventsSDK.on("Tick", () => {
 
 			const obs2ent = new Map<Obstacle, Entity>(),
 				start_pos = unit.Position.toVector2(),
-				base_ang = unit.Position.GetDirectionTo(pos).Angle
+				target_obs = new Obstacle(pos.toVector2(), target.HullRadius)
 			EntityManager.GetEntitiesByClasses<Unit>([Creep, Hero]).forEach(ent => {
-				if (ent !== unit && ((ent.IsAlive && ent.IsInRange(unit, hook.CastRange * 2) && !ent.IsInvulnerable && ent.IsVisible) || ent === target))
+				if (ent !== unit && ((ent.IsAlive && ent.IsInRange(unit, hook.CastRange * 2) && !ent.IsInvulnerable && ent.IsVisible && !ent.IsEnemy()) || ent === target))
 					obs2ent.set(
 						ent !== target
 							? MovingObstacle.FromUnit(ent)
-							: new Obstacle(pos.toVector2(), ent.HullRadius),
+							: target_obs,
 						ent
 					)
 			})
-			let obstacles = [...obs2ent.keys()]
-			let result = TryPredictInAngles(base_ang, -90, 90, start_pos, hook, obstacles, obs2ent, unit, target)
-			if (result !== undefined && respawn_time <= result[1]) {
-				unit.CastPosition(hook, unit.Position.Rotation(result[0], 300))
-				hook_sleeper.Sleep(Game.Ping + hook.CastPoint * 1000, unit)
-				if (lock_position.value)
-					hook_sleeper.Sleep(Game.Ping + hook.CastPoint * 1000, pos.LengthSqr)
-				return true
-			}
+			let predict_res = new NavMeshPathfinding(
+				new MovingObstacle(
+					start_pos/*.Add(angle.toVector2().MultiplyScalar(hook.AOERadius * 1.5))*/,
+					hook.AOERadius,
+					unit.Position.GetDirectionTo(pos).toVector2().MultiplyScalarForThis(hook.Speed),
+					hook.CastRange / hook.Speed,
+				),
+				[...obs2ent.keys()],
+				hook.CastPoint + unit.TurnTime(pos) + (autohook_delay.value / 60),
+			).GetFirstHitObstacle()
+			if (predict_res === undefined || predict_res[0] !== target_obs || respawn_time > predict_res[1])
+				return false
+			unit.CastPosition(hook, unit.Position.Extend(pos, hook.CastRange / 1.5))
+			hook_sleeper.Sleep(Game.Ping + hook.CastPoint * 1000, unit)
+			if (lock_position.value)
+				hook_sleeper.Sleep(Game.Ping + hook.CastPoint * 1000, pos.LengthSqr)
+			return true
 		})
 	})
 })
