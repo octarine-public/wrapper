51b24c1a7b6ae51cf19855cce913b4d3218d1131
LalkaKaro4 <storis2012@yandex.ru>
2020-01-15 22:48:53 +0300

    started Radiuses

---
+++ src_ts/Radiuses/base/RegisterModule.ts
@@ -0,0 +1,14 @@
+import { Entity } from "../../wrapper/Imports"
+
+export interface Module {
+	OnState: (state: boolean) => void
+
+	EntityCreated?: (ent: Entity) => void
+	EntityDestroyed?: (ent: Entity) => void
+
+	Draw?: () => void
+}
+
+export const Modules: Module[] = []
+
+export const RegisterModule = (module: Module) => Modules.push(module)
---
+++ src_ts/Radiuses/bootstrap.ts
@@ -0,0 +1,18 @@
+import { EventsSDK } from "wrapper/Imports"
+import { stateMain } from "./base/MenuBase"
+import { Modules } from "./base/RegisterModule"
+
+
+const OnState = (state: boolean) => Modules.forEach(module => module.OnState(state))
+
+
+stateMain.OnValue(self => OnState(self.value))
+
+EventsSDK.on("EntityCreated", ent => Modules.forEach(module => module.EntityCreated?.(ent)))
+
+EventsSDK.on("EntityDestroyed", ent => Modules.forEach(module => module.EntityDestroyed?.(ent)))
+
+EventsSDK.on("GameStarted", () => Modules.forEach(module => module.OnState(true)))
+EventsSDK.on("GameEnded", () => Modules.forEach(module => module.OnState(false)))
+
+EventsSDK.on("Draw", () => Modules.forEach(module => module.Draw?.()))
