71e76e33051dd5570ad16f3519b6efcd6e53ea23
LalkaKaro4 <storis2012@yandex.ru>
2020-01-15 22:51:48 +0300

    fixed bootstrap Radiuses

--- src_ts/Radiuses/bootstrap.ts
+++ src_ts/Radiuses/bootstrap.ts
@@ -5,14 +5,14 @@ import { Modules } from "./base/RegisterModule"
 
 const OnState = (state: boolean) => Modules.forEach(module => module.OnState(state))
 
-
 stateMain.OnValue(self => OnState(self.value))
 
-EventsSDK.on("EntityCreated", ent => Modules.forEach(module => module.EntityCreated?.(ent)))
-
+EventsSDK.on("EntityCreated", ent => stateMain.value
+	&& Modules.forEach(module => module.EntityCreated?.(ent)))
 EventsSDK.on("EntityDestroyed", ent => Modules.forEach(module => module.EntityDestroyed?.(ent)))
 
-EventsSDK.on("GameStarted", () => Modules.forEach(module => module.OnState(true)))
+EventsSDK.on("GameStarted", () => stateMain.value
+	&& Modules.forEach(module => module.OnState(true)))
 EventsSDK.on("GameEnded", () => Modules.forEach(module => module.OnState(false)))
 
 EventsSDK.on("Draw", () => Modules.forEach(module => module.Draw?.()))
