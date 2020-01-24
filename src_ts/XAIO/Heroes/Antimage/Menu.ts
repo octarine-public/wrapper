845b3261df991a9d9dbc89d71b4c35b4d5844ad0
SicdeX- <sicdex@gmail.com>
2020-01-25 05:06:30 +0600

    - remove AtiMage

--- src_ts/XAIO/Heroes/Antimage/Menu.ts
+++
@@ -1,9 +0,0 @@
-// import { XAIOMenuHero, XAIOSelectLanguage, XAIOMenuHeroesTree } from "XAIO/Menu/Menu"
-
-// export const {
-// 	XAIOState,
-// 	XAIOBaseTree,
-// 	XAIOComboKey,
-// 	XAIONearMouse,
-// 	XAIOComboTree,
-// } = XAIOMenuHero(XAIOMenuHeroesTree, "AntiMage")
