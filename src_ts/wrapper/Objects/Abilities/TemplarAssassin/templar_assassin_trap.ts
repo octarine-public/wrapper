39dab4e3ee7c8935d8c0e61e6e484996d914c57f
SicdeX- <sicdex@gmail.com>
2020-02-12 18:01:01 +0600

    added templar_assassin_trap (AOERadius)

--- src_ts/wrapper/Objects/Abilities/TemplarAssassin/templar_assassin_trap.ts
+++ src_ts/wrapper/Objects/Abilities/TemplarAssassin/templar_assassin_trap.ts
@@ -2,6 +2,10 @@ import Ability from "../../Base/Ability"
 
 export default class templar_assassin_trap extends Ability {
 	public NativeEntity: Nullable<C_DOTA_Ability_TemplarAssassin_Trap>
+
+	public get AOERadius(): number {
+		return this.GetSpecialValue("trap_radius")
+	}
 }
 
 import { RegisterClass } from "wrapper/Objects/NativeToSDK"
