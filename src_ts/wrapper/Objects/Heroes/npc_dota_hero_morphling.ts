0466eb8832f72769e4df17db09137602305f70c1
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2020-03-09 17:23:08 +0300

    Fixed morphling manabar offset

--- src_ts/wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
+++ src_ts/wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
@@ -8,6 +8,13 @@ export default class npc_dota_hero_morphling extends Hero {
 			return false
 		return super.IsIllusion
 	}
+
+	public get HealthBarOffset(): number {
+		let offset = super.HealthBarOffset
+		if (!this.HasBuffByName("modifier_morphling_replicate_manager"))
+			offset -= 10
+		return offset
+	}
 }
 
 import { RegisterClass } from "wrapper/Objects/NativeToSDK"
