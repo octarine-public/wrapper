f62dec731447f03af155084d816837b7262156dd
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2020-03-16 21:39:30 +0300

    Added temporary detailed debug for Manifest.GetPathByHash

--- Fusion-Native2.d.ts
+++ Fusion-Native2.d.ts
@@ -166,6 +166,7 @@ declare function TakeHeapSnapshot(path: string): void
 declare function hrtime(): number
 declare function AddSearchPath(path: string): boolean
 declare function RemoveSearchPath(path: string): boolean
+declare function HashToPath(hash: bigint): string | undefined
 declare function UnitNameIndexToString(index: number): string | undefined
 
 /// AUTOMATICALLY GENERATED
