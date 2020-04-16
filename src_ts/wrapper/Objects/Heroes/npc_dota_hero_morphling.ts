a46b08b95cc6f148118bee4264a885e7b3193422
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2020-04-16 18:45:34 +0300

    Added Bloodseeker's Rapture scepter support

--- src_ts/wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
+++ src_ts/wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
@@ -1,7 +1,6 @@
 import Hero from "../Base/Hero"
 
 export default class npc_dota_hero_morphling extends Hero {
-
 	public get IsIllusion(): boolean {
 		if (this.HasBuffByName("modifier_morphling_replicate"))
 			return false
