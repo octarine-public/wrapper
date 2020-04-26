a6c60b046370d47217983af62db7ade50b401084
SicdeX- <sicdex@gmail.com>
2020-04-26 22:40:55 +0600

    added tooltip BeAware IO MapHack

--- src_ts/BeAware/Module/WispMapHack/Menu.ts
+++ src_ts/BeAware/Module/WispMapHack/Menu.ts
@@ -1,4 +1,4 @@
 import { Menu } from "../../abstract/Menu.Base"
 
-let State = Menu.AddNode("MapHack").AddToggle("Wisp", true)
+let State = Menu.AddNode("MapHack").AddToggle("Wisp", true).SetTooltip("full MapHack wisp")
 export { State }
