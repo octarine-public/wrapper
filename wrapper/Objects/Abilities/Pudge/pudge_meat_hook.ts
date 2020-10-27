693b7cda5964a84f714afb9cc02e743fb737f45d
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2020-10-27 21:42:04 +0300

    Started declaring abilities/items classes in annotations

--- wrapper/Objects/Abilities/Pudge/pudge_meat_hook.ts
+++ wrapper/Objects/Abilities/Pudge/pudge_meat_hook.ts
@@ -2,6 +2,7 @@ import Ability from "../../Base/Ability"
 import { WrapperClass } from "../../../Decorators"
 
 @WrapperClass("pudge_meat_hook")
+@WrapperClass("CDOTA_Ability_Pudge_MeatHook")
 export default class pudge_meat_hook extends Ability {
 	public get AOERadius(): number {
 		return this.GetSpecialValue("hook_width")
