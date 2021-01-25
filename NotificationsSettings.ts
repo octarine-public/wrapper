439b99cdfaef1bee108e06131c989e331436f86f
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-01-25 11:00:44 +0300

    Removed unnecessary text from Notifications settings

--- NotificationsSettings.ts
+++ NotificationsSettings.ts
@@ -2,26 +2,26 @@ import { Menu as MenuSDK, NotificationsSDK } from "./wrapper/Imports"
 
 const Menu = MenuSDK.AddEntryDeep(["Settings", "Notifications"])
 Menu.AddSlider(
-	"Notifications size",
+	"Size",
 	NotificationsSDK.size,
 	50,
 	100,
 ).OnValue(slider => NotificationsSDK.size = slider.value)
 Menu.AddSlider(
-	"Notifications position",
+	"Position",
 	NotificationsSDK.y_offset,
 	200,
 	1000,
 ).OnValue(slider => NotificationsSDK.y_offset = slider.value)
 Menu.AddToggle(
-	"Show notifications positions",
+	"Show positions",
 	NotificationsSDK.debug,
 	"Enable if you want to change the size or position of notifications",
 ).OnValue(toggle => NotificationsSDK.debug = toggle.value)
 
 MenuSDK.Localization.AddLocalizationUnit("russian", new Map([
-	["Notifications size", "Размер уведомлений"],
-	["Notifications position", "Позиции уведомлений"],
-	["Show notifications positions", "Показать позиции уведомлений"],
+	["Size", "Размер"],
+	["Position", "Позиция"],
+	["Show positions", "Показать позиции"],
 	["Enable if you want to change the size or position of notifications", "Включите если хотите изменить размер или позицию уведомлений"],
 ]))
