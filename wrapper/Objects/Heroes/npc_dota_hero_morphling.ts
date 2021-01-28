847ea75464172c304ecfb0decf24e0ae911ee706
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-01-28 20:30:49 +0300

    Added crutch for morphling IsIllusion back

--- wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
+++ wrapper/Objects/Heroes/npc_dota_hero_morphling.ts
@@ -1,6 +1,19 @@
 import { WrapperClass } from "../../Decorators"
+import EntityManager from "../../Managers/EntityManager"
+import EventsSDK from "../../Managers/EventsSDK"
 import Hero from "../Base/Hero"
 
 @WrapperClass("C_DOTA_Unit_Hero_Morphling")
 export default class npc_dota_hero_morphling extends Hero {
+	public IsGuaranteedReal = false
+	public get isIllusion(): boolean {
+		return !this.IsGuaranteedReal && super.IsIllusion
+	}
 }
+
+EventsSDK.on("PostDataUpdate", () => {
+	EntityManager.GetEntitiesByClass(npc_dota_hero_morphling).forEach(hero => {
+		if (!hero.IsGuaranteedReal && !hero.IsIllusion)
+			hero.IsGuaranteedReal = true
+	})
+})
