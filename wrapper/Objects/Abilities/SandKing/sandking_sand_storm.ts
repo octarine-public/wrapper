75ffdf132ff38f70dac1c945c478da31ea4d5a17
SicdeX- <sicdex@gmail.com>
2021-01-30 18:14:19 +0600

    sandking_sand_storm#AOERadius

--- wrapper/Objects/Abilities/SandKing/sandking_sand_storm.ts
+++ wrapper/Objects/Abilities/SandKing/sandking_sand_storm.ts
@@ -3,4 +3,7 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("sandking_sand_storm")
 export default class sandking_sand_storm extends Ability {
+	public get AOERadius() {
+		return this.GetSpecialValue("sand_storm_radius")
+	}
 }
