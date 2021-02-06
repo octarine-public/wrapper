5619eaee433d32e528a8f8e5e5e5ea23512aa4a6
SicdeX- <sicdex@gmail.com>
2021-02-06 09:31:24 +0600

    phoenix_supernova#AOERadius

--- wrapper/Objects/Abilities/Phoenix/phoenix_supernova.ts
+++ wrapper/Objects/Abilities/Phoenix/phoenix_supernova.ts
@@ -3,4 +3,7 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("phoenix_supernova")
 export default class phoenix_supernova extends Ability {
+	public get AOERadius() {
+		return this.GetSpecialValue("aura_radius")
+	}
 }
