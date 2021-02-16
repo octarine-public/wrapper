8d5889089ffc88c929a7d2df70d6c01dd163524d
SicdeX- <sicdex@gmail.com>
2021-02-17 05:26:49 +0600

    add castrange & AOERadius shadowraze

--- wrapper/Objects/Abilities/Nevermore/nevermore_shadowraze2.ts
+++ wrapper/Objects/Abilities/Nevermore/nevermore_shadowraze2.ts
@@ -3,4 +3,10 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("nevermore_shadowraze2")
 export default class nevermore_shadowraze2 extends Ability {
+	public get AOERadius() {
+		return this.GetSpecialValue("shadowraze_radius")
+	}
+	public get BaseCastRange() {
+		return this.GetSpecialValue("shadowraze_range")
+	}
 }
--- wrapper/Objects/Abilities/Nevermore/nevermore_shadowraze3.ts
+++ wrapper/Objects/Abilities/Nevermore/nevermore_shadowraze3.ts
@@ -3,4 +3,10 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("nevermore_shadowraze3")
 export default class nevermore_shadowraze3 extends Ability {
+	public get AOERadius() {
+		return this.GetSpecialValue("shadowraze_radius")
+	}
+	public get BaseCastRange() {
+		return this.GetSpecialValue("shadowraze_range")
+	}
 }
