547b471d654e7e7e26343676c020c807059fc3b8
SicdeX- <sicdex@gmail.com>
2021-02-18 08:12:49 +0600

    add AbilityImmunityType in abyssal & arcane bolt

--- wrapper/Objects/Abilities/SkywrathMage/skywrath_mage_arcane_bolt.ts
+++ wrapper/Objects/Abilities/SkywrathMage/skywrath_mage_arcane_bolt.ts
@@ -15,6 +15,14 @@ export default class skywrath_mage_arcane_bolt extends Ability {
 		return damage
 	}
 
+	public get AbilityImmunityType(): SPELL_IMMUNITY_TYPES {
+		const talent = this.Owner?.GetAbilityByName("special_bonus_unique_skywrath_6")
+		if (talent === undefined || talent.Level === 0)
+			return this.AbilityImmunityType
+
+		return SPELL_IMMUNITY_TYPES.SPELL_IMMUNITY_ENEMIES_YES
+	}
+
 	public get ProjectileName() {
 		return [
 			"particles/units/heroes/hero_skywrath_mage/skywrath_mage_arcane_bolt.vpcf",
--- wrapper/Objects/Items/item_abyssal_blade.ts
+++ wrapper/Objects/Items/item_abyssal_blade.ts
@@ -3,5 +3,10 @@ import Item from "../Base/Item"
 
 @WrapperClass("item_abyssal_blade")
 export default class item_abyssal_blade extends Item {
+
 	public static readonly AppliesUnitState: modifierstate = modifierstate.MODIFIER_STATE_STUNNED
+
+	public get AbilityImmunityType(): SPELL_IMMUNITY_TYPES {
+		return SPELL_IMMUNITY_TYPES.SPELL_IMMUNITY_ENEMIES_YES
+	}
 }
