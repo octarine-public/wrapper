509c3955de97607d107bb9cc010c3fa7f891ae33
SicdeX- <sicdex@gmail.com>
2021-02-24 21:26:12 +0600

    fixed radius nevermore requiem

--- wrapper/Objects/Abilities/Nevermore/nevermore_requiem.ts
+++ wrapper/Objects/Abilities/Nevermore/nevermore_requiem.ts
@@ -4,6 +4,11 @@ import Ability from "../../Base/Ability"
 @WrapperClass("nevermore_requiem")
 export default class nevermore_requiem extends Ability {
 	public get AOERadius() {
-		return this.GetSpecialValue("requiem_radius")
+		return this.GetSpecialValue("requiem_radius") +
+			this.GetSpecialValue("requiem_line_width_start") +
+			this.GetSpecialValue("requiem_line_width_end")
+	}
+	public get Speed() {
+		return this.GetSpecialValue("requiem_line_speed")
 	}
 }
