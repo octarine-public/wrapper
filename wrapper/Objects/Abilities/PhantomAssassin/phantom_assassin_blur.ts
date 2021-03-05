fe00c03c8dfa5726dd5298c3088dd8bfb13107a2
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-03-06 02:38:28 +0300

    see desc
     - Added nevermore_dark_lord#AOERadius
     - Removed nevermore_dark_lord#CastRange
     - Added phantom_assassin_blur#AOERadius
     - Removed phantom_assassin_blur#CastRange
     - Removed snapfire_lil_shredder#CastRange

--- wrapper/Objects/Abilities/PhantomAssassin/phantom_assassin_blur.ts
+++ wrapper/Objects/Abilities/PhantomAssassin/phantom_assassin_blur.ts
@@ -3,4 +3,10 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("phantom_assassin_blur")
 export default class phantom_assassin_blur extends Ability {
+	public GetCastRangeForLevel(_level: number): number {
+		return 0
+	}
+	public GetAOERadiusForLevel(level: number): number {
+		return this.GetSpecialValue("radius", level)
+	}
 }
--- wrapper/Objects/Abilities/Snapfire/snapfire_lil_shredder.ts
+++ wrapper/Objects/Abilities/Snapfire/snapfire_lil_shredder.ts
@@ -3,4 +3,7 @@ import Ability from "../../Base/Ability"
 
 @WrapperClass("snapfire_lil_shredder")
 export default class snapfire_lil_shredder extends Ability {
+	public GetCastRangeForLevel(_level: number): number {
+		return 0
+	}
 }
