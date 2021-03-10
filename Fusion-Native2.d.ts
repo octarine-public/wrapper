364c115a0c9562c49e56c8c9db074acf3f6dc957
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-03-10 11:48:26 +0300

    Removed ToggleFakeChat

--- Fusion-Native2.d.ts
+++ Fusion-Native2.d.ts
@@ -152,7 +152,6 @@ declare function ChatWheelAbuse(str: string): void
 declare function StartFindingMatch(): void
 declare function SendGCPingResponse(): void
 declare function AcceptMatch(): void
-declare function ToggleFakeChat(state: boolean): void
 declare function ToggleOBSBypass(state: boolean): void
 declare function setFireEvent(func: (event_name: string, cancellable: boolean, ...args: any) => boolean): void
 declare function require(absolute_path: string): any
