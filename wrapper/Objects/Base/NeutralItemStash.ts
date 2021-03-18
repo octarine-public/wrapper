c922db6c1cb9d03bbf03c8292f76c8396fed5e74
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-03-19 00:08:53 +0300

    Added NeutralItemStash

---
+++ wrapper/Objects/Base/NeutralItemStash.ts
@@ -0,0 +1,6 @@
+import { WrapperClass } from "../../Decorators"
+import Building from "./Building"
+
+@WrapperClass("CDOTA_BaseNPC_NeutralItemStash")
+export default class NeutralItemStash extends Building {
+}
