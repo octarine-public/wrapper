a7bf4fd5bdfda219ccf16c9e7cb9120b2c23b558
SicdeX- <sicdex@gmail.com>
2021-03-18 23:44:57 +0600

    Added support for tinker_laser aghanims

--- wrapper/Objects/Abilities/Tinker/tinker_laser.ts
+++ wrapper/Objects/Abilities/Tinker/tinker_laser.ts
@@ -6,4 +6,12 @@ export default class tinker_laser extends Ability {
 	public get AbilityDamage(): number {
 		return this.GetSpecialValue("laser_damage")
 	}
+	public GetAOERadiusForLevel(level: number): number {
+		if (this.Owner?.HasScepter)
+			return this.GetSpecialValue("scepter_bounce_range", level)
+		return 0
+	}
+	public GetCastRangeForLevel(level: number): number {
+		return super.GetCastRangeForLevel(level) + this.GetAOERadiusForLevel(level)
+	}
 }
