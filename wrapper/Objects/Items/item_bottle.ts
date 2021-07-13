ae53da27c4f2b215c16da421cc9af44ec8ab22c7
moofMonkey <1892802-moofMonkey@users.noreply.gitlab.com>
2021-07-13 12:15:14 +0300

    Added item_bottle#TexturePath support

--- wrapper/Objects/Items/item_bottle.ts
+++ wrapper/Objects/Items/item_bottle.ts
@@ -20,6 +20,37 @@ export default class item_bottle extends Item {
 	public get TotalManaRestore(): number {
 		return this.GetSpecialValue("mana_restore") * this.Duration
 	}
+	public get TexturePath(): string {
+		switch (this.StoredRune) {
+			case DOTA_RUNES.DOTA_RUNE_DOUBLEDAMAGE:
+				return "panorama/images/items/bottle_doubledamage_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_HASTE:
+				return "panorama/images/items/bottle_haste_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_ILLUSION:
+				return "panorama/images/items/bottle_illusion_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_INVISIBILITY:
+				return "panorama/images/items/bottle_invisibility_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_REGENERATION:
+				return "panorama/images/items/bottle_regeneration_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_BOUNTY:
+				return "panorama/images/items/bottle_bounty_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_ARCANE:
+				return "panorama/images/items/bottle_arcane_png.vtex_c"
+			case DOTA_RUNES.DOTA_RUNE_XP:
+				return "panorama/images/items/bottle4_png.vtex_c"
+			default:
+				switch (this.CurrentCharges) {
+					case 2:
+						return "panorama/images/items/bottle_medium_png.vtex_c"
+					case 1:
+						return "panorama/images/items/bottle_small_png.vtex_c"
+					case 0:
+						return "panorama/images/items/bottle_empty_png.vtex_c"
+					default:
+						return this.AbilityData.TexturePath
+				}
+		}
+	}
 
 	public CanBeCasted(bonusMana: number = 0): boolean {
 		return this.CurrentCharges !== 0 && super.CanBeCasted(bonusMana)
