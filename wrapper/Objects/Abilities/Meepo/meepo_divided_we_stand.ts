58e2b08980cfe841abbb88ff37963b5cabae4d96
MoofMonkey <11695747+MoofMonkey@users.noreply.github.com>
2022-09-03 22:14:00 +0300

    Add "Divided We Stand" IsAlive crutch

--- wrapper/Objects/Abilities/Meepo/meepo_divided_we_stand.ts
+++ wrapper/Objects/Abilities/Meepo/meepo_divided_we_stand.ts
@@ -1,6 +1,8 @@
 import { NetworkedBasicField, WrapperClass } from "../../../Decorators"
 import EntityManager from "../../../Managers/EntityManager"
+import EventsSDK from "../../../Managers/EventsSDK"
 import Ability from "../../Base/Ability"
+import Unit from "../../Base/Unit"
 
 @WrapperClass("meepo_divided_we_stand")
 export default class meepo_divided_we_stand extends Ability {
@@ -20,3 +22,22 @@ export default class meepo_divided_we_stand extends Ability {
 		return EntityManager.EntityByIndex(this.NextDividedWeStand_) as Nullable<meepo_divided_we_stand>
 	}
 }
+
+let isInMeepoCrutch = false
+EventsSDK.on("LifeStateChanged", async ent => {
+	if (isInMeepoCrutch || !(ent instanceof Unit))
+		return
+	let abil = ent.GetAbilityByClass(meepo_divided_we_stand)
+	if (abil === undefined)
+		return
+	abil = abil.PrimeDividedWeStand
+	while (abil !== undefined) {
+		if (abil.Owner !== undefined && !abil.Owner.IsVisible) {
+			abil.Owner.LifeState = ent.LifeState
+			isInMeepoCrutch = true
+			await EventsSDK.emit("LifeStateChanged", false, abil.Owner)
+			isInMeepoCrutch = false
+		}
+		abil = abil.NextDividedWeStand
+	}
+})
