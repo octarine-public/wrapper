c8d1a320de78ef16f152b10eff7bd060c411812d
SicdeX <sicdex@gmail.com>
2023-05-04 23:24:21 +0600

    add new properties in Entity#RoshanSpawner

--- wrapper/Objects/Base/RoshanSpawner.ts
+++ wrapper/Objects/Base/RoshanSpawner.ts
@@ -1,5 +1,11 @@
-import { WrapperClass } from "../../Decorators"
+import { Vector3 } from "../../Base/Vector3"
+import { NetworkedBasicField, WrapperClass } from "../../Decorators"
 import { Entity } from "./Entity"
 
 @WrapperClass("CDOTA_RoshanSpawner")
-export class RoshanSpawner extends Entity {}
+export class RoshanSpawner extends Entity {
+	@NetworkedBasicField("m_iKillCount")
+	public readonly KillCount = 0
+	@NetworkedBasicField("m_vRoshanAltLocation")
+	public readonly AltLocation = new Vector3().Invalidate()
+}
