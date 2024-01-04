10ab17f56184ff1676aba1c80c8ab86879739f76
moofMonkey <11695747+moofMonkey@users.noreply.github.com>
2024-01-04 05:57:31 +0300

    Fix animations lookup

--- wrapper/Objects/Base/Entity.ts
+++ wrapper/Objects/Base/Entity.ts
@@ -586,13 +586,14 @@ export class Entity {
 
 		for (let i = 0; i < this.Animations.length; i++) {
 			const anim = this.Animations[i]
+			if (anim.activities.length !== modifiers.length) {
+				continue
+			}
 			let score = 0,
-				hasMovement = false,
 				foundEverything = true
 			for (let index = 0, end = anim.activities.length; index < end; index++) {
 				const activityData = anim.activities[index]
 				if (modifiers.includes(activityData.name)) {
-					hasMovement ||= activityData.name === "ACT_DOTA_RUN"
 					score += activityData.weight
 				} else {
 					foundEverything = false
@@ -602,9 +603,6 @@ export class Entity {
 			if (!foundEverything) {
 				continue
 			}
-			if (!hasMovement && anim.hasMovement && modifiers.includes("ACT_DOTA_RUN")) {
-				score += 1
-			}
 			if (score > highestScore) {
 				highestScore = score
 				highestScored = i
@@ -612,16 +610,14 @@ export class Entity {
 		}
 
 		// TODO: is this used anywhere? if so, is this correct?
-		if (highestScored !== undefined) {
-			for (let i = 0; i < this.Animations.length; i++) {
-				const anim = this.Animations[i]
-				if (
-					anim.activities.some(
-						activityData => activityData.name === "ACT_DOTA_CONSTANT_LAYER"
-					)
-				) {
-					return i
-				}
+		for (let i = 0; i < this.Animations.length; i++) {
+			const anim = this.Animations[i]
+			if (
+				anim.activities.some(
+					activityData => activityData.name === "ACT_DOTA_CONSTANT_LAYER"
+				)
+			) {
+				return i
 			}
 		}
 
