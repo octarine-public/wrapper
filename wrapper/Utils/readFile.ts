b66e7dfe0c2b070cf665a2c16b9d86c1de2c11d1
moofMonkey <11695747+moofMonkey@users.noreply.github.com>
2024-01-06 09:54:48 +0300

    Add tryFindFile fexists cache

--- wrapper/Utils/readFile.ts
+++ wrapper/Utils/readFile.ts
@@ -1,7 +1,14 @@
+const fexistsCache = new Set<string>(),
+	fexistsCacheInv = new Set<string>()
 export function tryFindFile(path: string, callstackDepth = 0): Nullable<string> {
-	if (fexists(path)) {
+	if (fexistsCache.has(path)) {
 		return path
 	}
+	if (!fexistsCacheInv.has(path) && fexists(path)) {
+		fexistsCache.add(path)
+		return path
+	}
+	fexistsCacheInv.add(path)
 
 	const callerStr = new Error().stack!.split("\n")[2 + callstackDepth]
 	const callerExec = /^\s{4}at\s(?:.+\s\()?(.+):\d+:\d+(?:\))?$/.exec(callerStr)
@@ -18,9 +25,14 @@ export function tryFindFile(path: string, callstackDepth = 0): Nullable<string> 
 	callerAr.pop() // remove script filename
 	while (callerAr.length !== 0) {
 		const currentPath = `${callerAr.join("/")}/scripts_files/${path}`
-		if (fexists(currentPath)) {
+		if (fexistsCache.has(currentPath)) {
 			return currentPath
 		}
+		if (!fexistsCacheInv.has(currentPath) && fexists(currentPath)) {
+			fexistsCache.add(currentPath)
+			return currentPath
+		}
+		fexistsCacheInv.add(currentPath)
 		callerAr.pop() // script file is probably lying in some folder, try traversing upwards
 	}
 
