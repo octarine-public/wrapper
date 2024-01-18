d3bd666d2176e85d990acfdaa4fd52d53a5705ef
SicdeX <sicdex@gmail.com>
2024-01-19 05:59:20 +0600

    see desc
    - improve array.clear()

--- internal/tests/index.ts
+++ internal/tests/index.ts
@@ -1,54 +1,63 @@
 // import { Menu } from "../../wrapper/Imports"
 
-// class Test {
-// 	public Primary = -1
-// 	public Secondary = -1
-// 	constructor(index: number, index2: number) {
-// 		this.Primary = index
-// 		this.Secondary = index2
-// 	}
-// }
+// // class Test {
+// // 	public Primary = -1
+// // 	public Secondary = -1
+// // 	constructor(index: number, index2: number) {
+// // 		this.Primary = index
+// // 		this.Secondary = index2
+// // 	}
+// // }
 
-/** one */
-// const arr = [
-// 	new Test(1, 2),
-// 	new Test(0, 1),
-// 	new Test(2, 1),
-// 	new Test(2, 0),
-// 	new Test(0, 2),
-// 	new Test(0, 3)
-// ]
+// /** one */
+// // const arr = [
+// // 	new Test(1, 2),
+// // 	new Test(0, 1),
+// // 	new Test(2, 1),
+// // 	new Test(2, 0),
+// // 	new Test(0, 2),
+// // 	new Test(0, 3)
+// // ]
 
-// const arr2 = [
-// 	new Test(1, 2),
-// 	new Test(0, 1),
-// 	new Test(2, 1),
-// 	new Test(2, 0),
-// 	new Test(0, 2),
-// 	new Test(0, 3)
-// ]
+// // const arr2 = [
+// // 	new Test(1, 2),
+// // 	new Test(0, 1),
+// // 	new Test(2, 1),
+// // 	new Test(2, 0),
+// // 	new Test(0, 2),
+// // 	new Test(0, 3)
+// // ]
+
+// // const arr = [...new Array(100_000).keys()]
+// // const arr2 = [...new Array(100_000).keys()]
 
 // const arr = [...new Array(100_000).keys()]
 // const arr2 = [...new Array(100_000).keys()]
-
 // const arr3 = [...new Array(100_000).keys()]
-// const arr4 = [...new Array(100_000).keys()]
-// const arr2 = [...new Array(100_000)]
 
 // const menu = Menu.AddEntry("Test")
 // menu.SortNodes = false
 
-// const key = menu.AddKeybind("Test 1")
-// const key2 = menu.AddKeybind("Test 2")
+// const key = menu.AddKeybind("Test length")
+// const key2 = menu.AddKeybind("Test splice")
+// const key3 = menu.AddKeybind("Test pop")
 
 // key.OnRelease(() => {
 // 	const start = hrtime()
-// 	// === test code
+// 	arr.length = 0
 // 	console.log(key.InternalName, hrtime() - start)
 // })
 
 // key2.OnRelease(() => {
 // 	const start = hrtime()
-// 	// === test code
+// 	arr2.splice(0, arr2.length)
 // 	console.log(key2.InternalName, hrtime() - start)
+// })
+
+// key3.OnRelease(() => {
+// 	const start = hrtime()
+// 	while (arr3.length > 0) {
+// 		arr3.pop()
+// 	}
+// 	console.log(key3.InternalName, hrtime() - start)
 // })
