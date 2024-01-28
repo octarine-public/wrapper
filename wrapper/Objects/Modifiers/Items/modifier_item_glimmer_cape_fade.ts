5836e4ab6c513c75cf5762640875a1cc8814a700
SicdeX <sicdex@gmail.com>
2024-01-28 22:31:33 +0600

    Fixed - IsShield (glimmer cape)

--- wrapper/Objects/Modifiers/Items/modifier_item_glimmer_cape_fade.ts
+++ wrapper/Objects/Modifiers/Items/modifier_item_glimmer_cape_fade.ts
@@ -2,4 +2,7 @@ import { WrapperClassModifier } from "../../../Decorators"
 import { Modifier } from "../../Base/Modifier"
 
 @WrapperClassModifier()
-export class modifier_item_glimmer_cape extends Modifier {}
+export class modifier_item_glimmer_cape_fade extends Modifier {
+	public readonly IsBuff = true
+	public readonly IsShield = true
+}
