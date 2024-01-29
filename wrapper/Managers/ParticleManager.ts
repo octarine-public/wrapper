8279873ada7cffd1f0694e53b5e415738dc481da
SicdeX <sicdex@gmail.com>
2024-01-29 21:09:47 +0600

    see desc
    - Fixed set alpha for range display
    - Added alpha for ParticlesSDK#DrawCircle
    - Fixed radius Ability#techies_minefield_sign

--- wrapper/Managers/ParticleManager.ts
+++ wrapper/Managers/ParticleManager.ts
@@ -131,7 +131,8 @@ export class ParticlesSDK {
 			[0, options.Position ?? entity],
 			[1, range],
 			[2, options.Color ?? Color.Aqua],
-			[3, Number(options.Fill ?? 0)]
+			[3, Number(options.Fill ?? 0)],
+			[4, options.Color?.a ?? 255]
 		)
 	}
 
--- wrapper/Objects/Abilities/Techies/techies_minefield_sign.ts
+++ wrapper/Objects/Abilities/Techies/techies_minefield_sign.ts
@@ -3,7 +3,18 @@ import { Ability } from "../../Base/Ability"
 
 @WrapperClass("techies_minefield_sign")
 export class techies_minefield_sign extends Ability {
+	/**
+	 * @param level
+	 * @return {number}
+	 */
 	public GetMaxCooldownForLevel(level: number): number {
 		return this.GetSpecialValue("AbilityCooldown", level)
+	}
+	/**
+	 * @param level
+	 * @return {number}
+	 */
+	public GetBaseAOERadiusForLevel(level: number): number {
+		return this.GetSpecialValue("trigger_radius", level)
 	}
 }
