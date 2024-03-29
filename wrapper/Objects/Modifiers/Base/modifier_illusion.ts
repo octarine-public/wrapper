84ab1dca146b68c48c3a72cf86737e301754f8c8
SicdeX <sicdex@gmail.com>
2024-03-29 15:32:10 +0600

    - Added illusion handling for SpiritBear

--- wrapper/Objects/Modifiers/Base/modifier_illusion.ts
+++ wrapper/Objects/Modifiers/Base/modifier_illusion.ts
@@ -1,7 +1,19 @@
 import { WrapperClassModifier } from "../../../Decorators"
+import { EventsSDK } from "../../../Managers/EventsSDK"
 import { Modifier } from "../../Base/Modifier"
 
 @WrapperClassModifier()
 export class modifier_illusion extends Modifier {
 	public readonly IsHidden = true
+
+	public UnitPropertyChanged(changed?: boolean): boolean {
+		const owner = this.Parent
+		const state = (changed ??= true)
+		if (owner === undefined || !owner.IsSpiritBear) {
+			return super.UnitPropertyChanged(changed)
+		}
+		owner.IsIllusion_ = state
+		EventsSDK.emit("UnitPropertyChanged", false, owner)
+		return super.UnitPropertyChanged(changed)
+	}
 }
