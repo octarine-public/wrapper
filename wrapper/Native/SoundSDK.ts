4b9f49705ddf92d05066a9004206b8a15e7c16bf
SicdeX <sicdex@gmail.com>
2024-04-19 21:46:58 +0600

    disable SoundEvent

--- wrapper/Native/SoundSDK.ts
+++ wrapper/Native/SoundSDK.ts
@@ -3,21 +3,24 @@ import { Entity } from "../Objects/Base/Entity"
 
 export const SoundSDK = new (class CSoundSDK {
 	public EmitStartSoundEvent(
-		name: string,
-		position = new Vector3().Invalidate(),
-		sourceEntity?: Entity,
-		guid = (Math.random() * (2 ** 32 - 1)) | 0,
-		seed = (Math.random() * (2 ** 32 - 1)) | 0
+		_name: string,
+		_position = new Vector3().Invalidate(),
+		_sourceEntity?: Entity,
+		_guid = (Math.random() * (2 ** 32 - 1)) | 0,
+		_seed = (Math.random() * (2 ** 32 - 1)) | 0
 	): void {
-		position.toIOBuffer()
-		EmitStartSoundEvent(guid, name, sourceEntity?.Index ?? -1, seed)
+		// uncomment after fix crashes
+		// position.toIOBuffer()
+		// EmitStartSoundEvent(guid, name, sourceEntity?.Index ?? -1, seed)
 	}
 
-	public EmitStopSoundEvent(guid: number): void {
-		EmitStopSoundEvent(guid, undefined, -1)
+	public EmitStopSoundEvent(_guid: number): void {
+		// uncomment after fix crashes
+		// EmitStopSoundEvent(guid, undefined, -1)
 	}
 
-	public EmitStopSoundEventByName(name: string, sourceEntity?: Entity): void {
-		EmitStopSoundEvent(0, name, sourceEntity?.Index ?? -1)
+	public EmitStopSoundEventByName(_name: string, _sourceEntity?: Entity): void {
+		// uncomment after fix crashes
+		// EmitStopSoundEvent(0, name, sourceEntity?.Index ?? -1)
 	}
 })()
