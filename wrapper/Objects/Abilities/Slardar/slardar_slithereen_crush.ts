ca53f7de601634081f72b2815decd58a35365e19
SicdeX <sicdex@gmail.com>
2024-05-18 02:33:42 +0600

    Fixed Slardar SlithereenCrush#AOERadius

--- wrapper/Objects/Abilities/Slardar/slardar_slithereen_crush.ts
+++ wrapper/Objects/Abilities/Slardar/slardar_slithereen_crush.ts
@@ -4,6 +4,9 @@ import { Ability } from "../../Base/Ability"
 @WrapperClass("slardar_slithereen_crush")
 export class slardar_slithereen_crush extends Ability {
 	public GetBaseAOERadiusForLevel(level: number): number {
-		return this.GetSpecialValue("crush_radius", level)
+		return (
+			this.GetSpecialValue("crush_radius", level) +
+			this.GetSpecialValue("shard_bonus_radius", level)
+		)
 	}
 }
