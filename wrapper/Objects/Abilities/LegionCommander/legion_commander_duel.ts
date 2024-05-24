004a5eb750c45c680183f37eda1fbf166e201a65
SicdeX <sicdex@gmail.com>
2024-05-24 23:15:47 +0600

    Fixed Legion Commander duel

--- wrapper/Objects/Abilities/LegionCommander/legion_commander_duel.ts
+++ wrapper/Objects/Abilities/LegionCommander/legion_commander_duel.ts
@@ -3,7 +3,13 @@ import { Ability } from "../../Base/Ability"
 
 @WrapperClass("legion_commander_duel")
 export class legion_commander_duel extends Ability {
+	public GetMaxDurationForLevel(level: number): number {
+		return this.GetSpecialValue("duration", level)
+	}
 	public GetMaxCooldownForLevel(level: number): number {
 		return this.GetSpecialValue("AbilityCooldown", level)
+	}
+	public GetBaseCastRangeForLevel(level: number): number {
+		return this.GetSpecialValue("AbilityCastRange", level)
 	}
 }
