54690b5ad17da737a523cc8e275cba0568e09a02
SicdeX <sicdex@gmail.com>
2024-05-28 22:08:58 +0600

    Fixed Anti Mage mana overload

--- wrapper/Objects/Abilities/AntiMage/antimage_mana_overload.ts
+++ wrapper/Objects/Abilities/AntiMage/antimage_mana_overload.ts
@@ -2,4 +2,22 @@ import { WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
 @WrapperClass("antimage_mana_overload")
-export class antimage_mana_overload extends Ability {}
+export class antimage_mana_overload extends Ability {
+	public GetBaseCastRangeForLevel(level: number): number {
+		const owner = this.Owner
+		if (owner === undefined) {
+			return 0
+		}
+		let value = super.GetBaseCastRangeForLevel(level)
+		if (value === 0 && level !== 0) {
+			value =
+				owner
+					.GetAbilityByName("antimage_blink")
+					?.GetBaseCastRangeForLevel(level) ?? 0
+		}
+		return value
+	}
+	public GetMaxDurationForLevel(level: number): number {
+		return this.GetSpecialValue("duration", level)
+	}
+}
