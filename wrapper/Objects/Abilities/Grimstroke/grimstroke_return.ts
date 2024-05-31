adafd9c8ee45f84a151a8fd5ea2ce776a77a7570
SicdeX <sicdex@gmail.com>
2024-05-31 23:13:26 +0600

    Fixed Grimstroke return

--- wrapper/Objects/Abilities/Grimstroke/grimstroke_return.ts
+++ wrapper/Objects/Abilities/Grimstroke/grimstroke_return.ts
@@ -2,4 +2,13 @@ import { WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
 @WrapperClass("grimstroke_return")
-export class grimstroke_return extends Ability {}
+export class grimstroke_return extends Ability {
+	public GetBaseAOERadiusForLevel(level: number): number {
+		const owner = this.Owner
+		if (owner === undefined) {
+			return 0
+		}
+		const ability = owner.GetAbilityByName("grimstroke_spirit_walk")
+		return ability?.GetBaseAOERadiusForLevel(level) ?? 0
+	}
+}
