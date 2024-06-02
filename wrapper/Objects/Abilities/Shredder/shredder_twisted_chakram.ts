6cd6da8be1b72789143cecf4c1f7a0a04f05df95
SicdeX <sicdex@gmail.com>
2024-06-02 20:04:54 +0600

    Fix Twisted chakram damage, speed, radius

--- wrapper/Objects/Abilities/Shredder/shredder_twisted_chakram.ts
+++ wrapper/Objects/Abilities/Shredder/shredder_twisted_chakram.ts
@@ -2,4 +2,14 @@ import { WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
 @WrapperClass("shredder_twisted_chakram")
-export class shredder_twisted_chakram extends Ability {}
+export class shredder_twisted_chakram extends Ability {
+	public GetBaseSpeedForLevel(level: number): number {
+		return this.GetSpecialValue("speed", level)
+	}
+	public GetBaseDamageForLevel(level: number): number {
+		return this.GetSpecialValue("damage", level)
+	}
+	public GetBaseAOERadiusForLevel(level: number): number {
+		return this.GetSpecialValue("radius", level)
+	}
+}
