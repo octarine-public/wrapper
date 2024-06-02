5309723889ea3142fa906b8b4ef7042d33ed5736
SicdeX <sicdex@gmail.com>
2024-06-02 22:21:04 +0600

    Move general fonts to wrapper

---
+++ internal/typography/fonts-jp.ts
@@ -0,0 +1,5 @@
+// https://github.com/octarine-public/fonts-jp
+
+// import { LoadFontWrapper } from "./helper"
+
+// LoadFontWrapper("fonts/MPLUS1/MPLUS1-VariableFont_wght.ttf", true)
---
+++ internal/typography/fonts-zh.ts
@@ -0,0 +1,9 @@
+// https://github.com/octarine-public/fonts-zh
+
+// import { LoadFontWrapper } from "./helper"
+
+// LoadFontWrapper("fonts/NotoSansTC/NotoSansTC-Bold.otf")
+// LoadFontWrapper("fonts/NotoSansTC/NotoSansTC-Regular.otf", true)
+
+// LoadFontWrapper("fonts/NotoSansSC/NotoSansSC-Bold.otf")
+// LoadFontWrapper("fonts/NotoSansSC/NotoSansSC-Regular.otf", true)
