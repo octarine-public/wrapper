ee7109462756733f13c86ae1db2a87ae415fc9b4
SicdeX <sicdex@gmail.com>
2024-06-06 09:03:28 +0600

    see desc
    - Update Arc Warden spark wraith
    - Added new modifier

--- wrapper/Objects/Abilities/ArcWarden/arc_warden_spark_wraith.ts
+++ wrapper/Objects/Abilities/ArcWarden/arc_warden_spark_wraith.ts
@@ -6,4 +6,23 @@ export class arc_warden_spark_wraith extends Ability {
 	public GetBaseAOERadiusForLevel(level: number): number {
 		return this.GetSpecialValue("radius", level)
 	}
+	public GetMaxDurationForLevel(level: number): number {
+		return this.GetSpecialValue("duration", level)
+	}
+	public GetBaseActivationDelayForLevel(level: number): number {
+		const owner = this.Owner
+		if (owner === undefined) {
+			return 0
+		}
+		const specialName = `${owner.IsClone ? "tempest" : "base"}_activation_delay`
+		return this.GetSpecialValue(specialName, level)
+	}
+	public GetBaseDamageForLevel(level: number): number {
+		const owner = this.Owner
+		if (owner === undefined) {
+			return 0
+		}
+		const specialName = `spark_damage_${owner.IsClone ? "tempest" : "base"}`
+		return this.GetSpecialValue(specialName, level)
+	}
 }
