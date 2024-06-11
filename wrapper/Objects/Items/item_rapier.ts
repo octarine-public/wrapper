bbb17f2e0986911c3e2eccfed64b25907e87589b
SicdeX <sicdex@gmail.com>
2024-06-11 12:14:53 +0600

    Fixed Rapier#TexturePath

--- wrapper/Objects/Items/item_rapier.ts
+++ wrapper/Objects/Items/item_rapier.ts
@@ -1,5 +1,17 @@
+import { Paths } from "../../Data/ImageData"
 import { WrapperClass } from "../../Decorators"
+import { DOTA_ABILITY_BEHAVIOR } from "../../Enums/DOTA_ABILITY_BEHAVIOR"
 import { Item } from "../Base/Item"
 
 @WrapperClass("item_rapier")
-export class item_rapier extends Item {}
+export class item_rapier extends Item {
+	public get TexturePath(): string {
+		if (
+			!this.IsToggled ||
+			!this.HasBehavior(DOTA_ABILITY_BEHAVIOR.DOTA_ABILITY_BEHAVIOR_TOGGLE)
+		) {
+			return super.TexturePath
+		}
+		return Paths.ItemIcons + "/rapier_alt_png.vtex_c"
+	}
+}
