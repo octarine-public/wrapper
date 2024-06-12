38cade5a31100a7c3f0c790e5f4c69b2b4b0f6dd
SicdeX <sicdex@gmail.com>
2024-06-12 12:45:13 +0600

    Fixed modifiers

--- wrapper/Objects/Modifiers/Abilities/BountyHunter/modifier_bounty_hunter_wind_walk_slow.ts
+++ wrapper/Objects/Modifiers/Abilities/BountyHunter/modifier_bounty_hunter_wind_walk_slow.ts
@@ -4,8 +4,4 @@ import { Modifier } from "../../../Base/Modifier"
 @WrapperClassModifier()
 export class modifier_bounty_hunter_wind_walk_slow extends Modifier {
 	public readonly IsDebuff = true
-
-	protected SetBonusAttackSpeed(specialName = "attack_slow", subtract = true): void {
-		super.SetBonusAttackSpeed(specialName, subtract)
-	}
 }
