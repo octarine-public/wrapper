8038751a95c39024fa359496eb1266e655202868
SicdeX <sicdex@gmail.com>
2024-07-09 19:16:52 +0600

    see desc
    - Fixed invoker chaos meteor (speed)
    - Fixed invoker chaos meteor ad (speed)

--- wrapper/Objects/Abilities/Invoker/invoker_chaos_meteor.ts
+++ wrapper/Objects/Abilities/Invoker/invoker_chaos_meteor.ts
@@ -1,5 +1,8 @@
 import { NetworkedBasicField, WrapperClass } from "../../../Decorators"
+import { EntityManager } from "../../../Managers/EntityManager"
+import { EventsSDK } from "../../../Managers/EventsSDK"
 import { Ability } from "../../Base/Ability"
+import { LinearProjectile } from "../../Base/Projectile"
 
 @WrapperClass("invoker_chaos_meteor")
 export class invoker_chaos_meteor extends Ability {
@@ -10,8 +13,11 @@ export class invoker_chaos_meteor extends Ability {
 	@NetworkedBasicField("m_nExortLevel")
 	public ExortLevel = 0
 
+	/** @ignore */
+	public HasProjectile_ = false
+
 	public get LandTime() {
-		return this.GetSpecialValue("land_time")
+		return this.ActivationDelay
 	}
 
 	public get Speed() {
@@ -23,10 +29,32 @@ export class invoker_chaos_meteor extends Ability {
 	}
 
 	public GetBaseSpeedForLevel(level: number): number {
-		return this.GetSpecialValue("travel_speed", level)
+		return this.HasProjectile_ ? this.GetSpecialValue("travel_speed", level) : 0
 	}
 
 	public GetBaseDamageForLevel(level: number): number {
 		return this.GetSpecialValue("main_damage", level)
 	}
+
+	public GetBaseActivationDelayForLevel(level: number): number {
+		return this.GetSpecialValue("land_time", level)
+	}
 }
+
+// TODO: fix me, move to event obstacle types
+const path = "particles/units/heroes/hero_invoker/invoker_chaos_meteor.vpcf"
+function UpdateSpeed(proj: LinearProjectile) {
+	if (proj.ParticlePathNoEcon !== path) {
+		return
+	}
+	const abilities = EntityManager.GetEntitiesByClass(invoker_chaos_meteor)
+	for (let index = abilities.length - 1; index > -1; index--) {
+		const ability = abilities[index]
+		if (proj.Source === ability.Owner) {
+			ability.HasProjectile_ = !ability.HasProjectile_
+		}
+	}
+}
+
+EventsSDK.on("LinearProjectileCreated", proj => UpdateSpeed(proj))
+EventsSDK.on("LinearProjectileDestroyed", proj => UpdateSpeed(proj))
--- wrapper/Objects/Abilities/Invoker/invoker_chaos_meteor_ad.ts
+++ wrapper/Objects/Abilities/Invoker/invoker_chaos_meteor_ad.ts
@@ -1,5 +1,8 @@
 import { NetworkedBasicField, WrapperClass } from "../../../Decorators"
+import { EntityManager } from "../../../Managers/EntityManager"
+import { EventsSDK } from "../../../Managers/EventsSDK"
 import { Ability } from "../../Base/Ability"
+import { LinearProjectile } from "../../Base/Projectile"
 
 @WrapperClass("invoker_chaos_meteor_ad")
 export class invoker_chaos_meteor_ad extends Ability {
@@ -9,4 +12,27 @@ export class invoker_chaos_meteor_ad extends Ability {
 	public WexLevel = 0
 	@NetworkedBasicField("m_nExortLevel")
 	public ExortLevel = 0
+	public HasProjectile_ = false
+
+	public GetBaseSpeedForLevel(level: number): number {
+		return this.HasProjectile_ ? this.GetSpecialValue("travel_speed", level) : 0
+	}
 }
+
+// TODO: fix me, move to event obstacle types
+const path = "particles/units/heroes/hero_invoker/invoker_chaos_meteor.vpcf"
+function UpdateSpeed(proj: LinearProjectile) {
+	if (proj.ParticlePathNoEcon !== path) {
+		return
+	}
+	const abilities = EntityManager.GetEntitiesByClass(invoker_chaos_meteor_ad)
+	for (let index = abilities.length - 1; index > -1; index--) {
+		const ability = abilities[index]
+		if (proj.Source === ability.Owner) {
+			ability.HasProjectile_ = !ability.HasProjectile_
+		}
+	}
+}
+
+EventsSDK.on("LinearProjectileCreated", proj => UpdateSpeed(proj))
+EventsSDK.on("LinearProjectileDestroyed", proj => UpdateSpeed(proj))
