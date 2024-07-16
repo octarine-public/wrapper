08e04d4802fe9a971761382abe1585cf44a0e538
SicdeX <sicdex@gmail.com>
2024-07-17 04:32:10 +0600

    see desc
    - Fixed Chaos Knight chaos_bolt#GetBaseCastRangeForLevel
    - Fixed Modifier kaya and sange

--- wrapper/Objects/Abilities/ChaosKnight/chaos_knight_chaos_bolt.ts
+++ wrapper/Objects/Abilities/ChaosKnight/chaos_knight_chaos_bolt.ts
@@ -6,14 +6,6 @@ export class chaos_knight_chaos_bolt extends Ability {
 	public get ProjectileAttachment(): string {
 		return "attach_attack1"
 	}
-	public GetBaseCastRangeForLevel(level: number): number {
-		return (
-			super.GetBaseCastRangeForLevel(level) +
-			(this.Owner?.HasShard ?? false
-				? this.GetSpecialValue("shard_bonus_cast_range", level)
-				: 0)
-		)
-	}
 	public GetBaseSpeedForLevel(level: number): number {
 		return this.GetSpecialValue("chaos_bolt_speed", level)
 	}
--- wrapper/Objects/Modifiers/Items/modifier_item_kaya_and_sange.ts
+++ wrapper/Objects/Modifiers/Items/modifier_item_kaya_and_sange.ts
@@ -3,13 +3,6 @@ import { Modifier } from "../../Base/Modifier"
 
 @WrapperClassModifier()
 export class modifier_item_kaya_and_sange extends Modifier {
-	protected SetStatusResistanceAmplifier(
-		specialName = "status_resistance",
-		subtract = false
-	) {
-		super.SetStatusResistanceAmplifier(specialName, subtract)
-	}
-
 	public SetBonusManaCostAmplifier(
 		specialName = "manacost_reduction",
 		subtract = true
