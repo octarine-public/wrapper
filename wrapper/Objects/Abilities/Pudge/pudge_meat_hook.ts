3a38bc7949864f12e3d35607abd93b4ac6e274aa
SicdeX <sicdex@gmail.com>
2024-07-20 02:43:12 +0600

    Fixed pudge hook

--- wrapper/Objects/Abilities/Pudge/pudge_meat_hook.ts
+++ wrapper/Objects/Abilities/Pudge/pudge_meat_hook.ts
@@ -1,21 +1,18 @@
 import { NetworkedBasicField, WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
-@WrapperClass("pudge_meat_hook")
 @WrapperClass("CDOTA_Ability_Pudge_MeatHook")
 export class pudge_meat_hook extends Ability {
 	@NetworkedBasicField("m_nConsecutiveHits")
 	public ConsecutiveHits = 0
 
-	public get Speed(): number {
-		return this.GetSpecialValue("hook_speed")
-	}
-
 	public GetBaseDamageForLevel(level: number): number {
 		return this.GetSpecialValue("damage", level)
 	}
-
 	public GetBaseAOERadiusForLevel(level: number): number {
 		return this.GetSpecialValue("hook_width", level)
+	}
+	public GetBaseSpeedForLevel(level: number): number {
+		return this.GetSpecialValue("hook_speed", level)
 	}
 }
