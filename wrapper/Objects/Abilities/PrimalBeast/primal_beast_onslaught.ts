f8925042538a694d515c738b41021a8862f1ab09
SicdeX <sicdex@gmail.com>
2024-08-02 16:04:07 +0600

    Fixed Primal beast onslaught

--- wrapper/Objects/Abilities/PrimalBeast/primal_beast_onslaught.ts
+++ wrapper/Objects/Abilities/PrimalBeast/primal_beast_onslaught.ts
@@ -1,13 +1,18 @@
 import { WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
-@WrapperClass("CDOTA_Ability_PrimalBeast_Onslaught")
+@WrapperClass("primal_beast_onslaught")
 export class primal_beast_onslaught extends Ability {
+	public GetBaseCastRangeForLevel(level: number): number {
+		return this.GetSpecialValue("max_distance", level)
+	}
 	public GetBaseSpeedForLevel(level: number): number {
 		return this.GetSpecialValue("charge_speed", level)
 	}
-
 	public GetBaseDamageForLevel(level: number): number {
 		return this.GetSpecialValue("knockback_damage", level)
+	}
+	public GetBaseAOERadiusForLevel(level: number): number {
+		return this.GetSpecialValue("knockback_radius", level)
 	}
 }
