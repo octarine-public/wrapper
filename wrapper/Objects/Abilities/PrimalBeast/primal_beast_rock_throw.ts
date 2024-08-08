98b48a26a2b3fe79073606a3535dd150a7cee775
Ispusinov Artur Alekseevich <okolobaxha@yandex.ru>
2024-08-08 15:38:26 +0700

    fix: primal beast rock throw

--- wrapper/Objects/Abilities/PrimalBeast/primal_beast_rock_throw.ts
+++ wrapper/Objects/Abilities/PrimalBeast/primal_beast_rock_throw.ts
@@ -1,5 +1,5 @@
 import { WrapperClass } from "../../../Decorators"
 import { Ability } from "../../Base/Ability"
 
-@WrapperClass("CDOTA_Ability_PrimalBeast_Rock_Throw")
+@WrapperClass("primal_beast_rock_throw")
 export class primal_beast_rock_throw extends Ability {}
