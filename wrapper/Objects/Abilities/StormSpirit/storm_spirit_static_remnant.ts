47a9529f8d404ef99fd57b30cb2c13f549091fbb
moofMonkey <11695747+moofMonkey@users.noreply.github.com>
2024-11-02 09:21:04 +0300

    Fix storm_spirit_static_remnant#AbilityBehaviorMask with Static Slide facet

--- wrapper/Objects/Abilities/StormSpirit/storm_spirit_static_remnant.ts
+++ wrapper/Objects/Abilities/StormSpirit/storm_spirit_static_remnant.ts
@@ -1,8 +1,19 @@
 import { WrapperClass } from "../../../Decorators"
+import { DOTA_ABILITY_BEHAVIOR } from "../../../Enums/DOTA_ABILITY_BEHAVIOR"
 import { Ability } from "../../Base/Ability"
+import { Hero } from "../../Base/Hero"
 
 @WrapperClass("storm_spirit_static_remnant")
 export class storm_spirit_static_remnant extends Ability {
+	public get AbilityBehaviorMask(): DOTA_ABILITY_BEHAVIOR {
+		const owner = this.Owner
+		if (!(owner instanceof Hero)) {
+			return super.AbilityBehaviorMask
+		}
+		return owner.HeroFacetID !== 2 || this.AltCastState
+			? super.AbilityBehaviorMask
+			: DOTA_ABILITY_BEHAVIOR.DOTA_ABILITY_BEHAVIOR_POINT
+	}
 	public GetBaseAOERadiusForLevel(level: number): number {
 		return this.GetSpecialValue("static_remnant_radius", level)
 	}
