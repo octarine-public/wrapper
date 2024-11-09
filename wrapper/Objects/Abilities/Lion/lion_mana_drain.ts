6cff5e5845113e11440d600cc57ec4f58ecbc167
SicdeX <sicdex@gmail.com>
2024-11-10 03:55:32 +0600

    fix: lion_mana_drain#TargetTeamMask

--- wrapper/Objects/Abilities/Lion/lion_mana_drain.ts
+++ wrapper/Objects/Abilities/Lion/lion_mana_drain.ts
@@ -1,5 +1,10 @@
 import { WrapperClass } from "../../../Decorators"
+import { DOTA_UNIT_TARGET_TEAM } from "../../../Enums/DOTA_UNIT_TARGET_TEAM"
 import { Ability } from "../../Base/Ability"
 
 @WrapperClass("lion_mana_drain")
-export class lion_mana_drain extends Ability {}
+export class lion_mana_drain extends Ability {
+	public get TargetTeamMask(): DOTA_UNIT_TARGET_TEAM {
+		return super.TargetTeamMask | DOTA_UNIT_TARGET_TEAM.DOTA_UNIT_TARGET_TEAM_BOTH
+	}
+}
