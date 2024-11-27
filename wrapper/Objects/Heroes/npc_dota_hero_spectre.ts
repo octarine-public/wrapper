a8192661d5fc2a031db054223d09835b89cbf6cf
SicdeX <sicdex@gmail.com>
2024-11-27 18:54:07 +0600

    fix: syntax error

--- wrapper/Objects/Heroes/npc_dota_hero_spectre.ts
+++ wrapper/Objects/Heroes/npc_dota_hero_spectre.ts
@@ -7,7 +7,8 @@ export class npc_dota_hero_spectre extends Hero {
 	public CalculateActivityModifiers(activity: GameActivity, ar: string[]): void {
 		super.CalculateActivityModifiers(activity, ar)
 		if (this.MyWearables.some(wearable => wearable.ItemDefinitionIndex === 9662)) {
-			if (this.Level >= 22) { ar.push("daggers_5") }
+			if (this.Level >= 22) {
+				ar.push("daggers_5")
 			} else if (this.Level >= 15) {
 				ar.push("daggers_4")
 			} else if (this.Level >= 8) {
