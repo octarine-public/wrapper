2f6d0a2726b20f0e63310f4a7cf7779de23f72e8
SicdeX <sicdex@gmail.com>
2025-02-04 00:01:03 +0600

    fix: modifier charge of darkness target

--- wrapper/Objects/Modifiers/Abilities/SpiritBreaker/modifier_spirit_breaker_charge_of_darkness_target.ts
+++ wrapper/Objects/Modifiers/Abilities/SpiritBreaker/modifier_spirit_breaker_charge_of_darkness_target.ts
@@ -1,56 +1,18 @@
 import { GetUnitTexture } from "../../../../Data/ImageData"
 import { WrapperClassModifier } from "../../../../Decorators"
+import { EntityManager } from "../../../../Managers/EntityManager"
 import { Modifier } from "../../../Base/Modifier"
-import { Units } from "../../../Base/Unit"
+import { Unit } from "../../../Base/Unit"
 
 @WrapperClassModifier()
 export class modifier_spirit_breaker_charge_of_darkness_target extends Modifier {
-	private targetName: Nullable<string>
-
 	public get ForceVisible(): boolean {
 		return true
 	}
+	public get Target() {
+		return EntityManager.EntityByIndex<Unit>(this.NetworkArmor)
+	}
 	public GetTexturePath(): string {
-		return GetUnitTexture(this.targetName ?? "") ?? ""
-	}
-
-	public UnitPropertyChanged(_changed?: boolean): boolean {
-		const owner = this.Parent
-		if (owner === undefined) {
-			return super.UnitPropertyChanged(_changed)
-		}
-		this.targetName = this.GetTarget()?.Name
-		return super.UnitPropertyChanged(_changed)
-	}
-
-	public AddModifier(): boolean {
-		if (!super.AddModifier()) {
-			return false
-		}
-		this.targetName = this.GetTarget()?.Name
-		return true
-	}
-
-	public Remove(): boolean {
-		if (!super.Remove()) {
-			return false
-		}
-		this.targetName = this.GetTarget()?.Name
-		return true
-	}
-
-	private GetTarget() {
-		return Units.find(x => {
-			if (!x.IsAlive || x.IsBuilding || x.IsCourier) {
-				return false
-			}
-			const modifier = x.GetBuffByName(
-				"modifier_spirit_breaker_charge_of_darkness_vision"
-			)
-			if (modifier === undefined || modifier.Caster !== this.Parent) {
-				return false
-			}
-			return modifier !== undefined && modifier.Caster === this.Parent
-		})
+		return GetUnitTexture(this.Target?.Name ?? "") ?? ""
 	}
 }
