82617bae4abe084ec568e87393cd7216563394d3
SicdeX <sicdex@gmail.com>
2025-03-30 10:20:33 +0600

    add: RGB trail mouse to home menu

--- internal/main-menu/Home/index.ts
+++ internal/main-menu/Home/index.ts
@@ -1,10 +1,15 @@
 import {
+	Color,
 	ConVarsSDK,
 	Events,
 	EventsSDK,
+	InputManager,
+	MathSDK,
 	Menu,
 	MenuLanguageID,
-	SOType
+	RendererSDK,
+	SOType,
+	Vector2
 } from "../../../wrapper/Imports"
 
 const enum CSODOTALobbyState {
@@ -17,10 +22,15 @@ const enum CSODOTALobbyState {
 	SERVERASSIGN = 6
 }
 
-/** Main menu manager  */
 new (class CInternalMainMenu {
+	private trailHue = 0
 	private acceptTime = -1
+	private maxTrailLength = 50
+	private trailPositions: Vector2[] = []
+
 	private readonly tree = Menu.AddEntry("Main")
+	private readonly rgbTrail = this.tree.AddToggle("RGB trail", false, "RGB mouse trail")
+
 	private readonly acceptDelay = this.tree.AddSlider(
 		"AutoAccept delay",
 		26,
@@ -29,27 +39,23 @@ new (class CInternalMainMenu {
 		0,
 		"Delay on auto accept,\nyou can disable it by setting slider to max value"
 	)
-	private readonly fowConvars = this.tree.AddToggle(
-		"FoW hacks",
-		true,
-		"Such as seeing TPs in the world, having no fog, etc"
-	)
 
 	constructor() {
-		EventsSDK.on("Draw", this.Draw.bind(this))
-		Events.on("SetLanguage", this.SetLanguage.bind(this))
-		EventsSDK.on("SharedObjectChanged", this.SharedObjectChanged.bind(this))
-
 		this.tree
 			.AddToggle("Trigger keybinds in chat", false)
 			.OnValue(toggle => (Menu.Base.triggerOnChat = toggle.value))
+
+		EventsSDK.on("Draw", this.Draw.bind(this))
+		Events.on("SetLanguage", this.SetLanguage.bind(this))
+		EventsSDK.on("SharedObjectChanged", this.SharedObjectChanged.bind(this))
 	}
 
 	protected Draw(): void {
+		this.trailMouse()
 		this.updateAutoAccept()
 		ConVarsSDK.Set("fog_override", 0)
+		ConVarsSDK.Set("fog_enable", false)
 		ConVarsSDK.Set("fow_client_visibility", 0)
-		ConVarsSDK.Set("fog_enable", !this.fowConvars.value)
 	}
 
 	protected SharedObjectChanged(
@@ -89,5 +95,43 @@ new (class CInternalMainMenu {
 		}
 		AcceptMatch()
 		this.acceptTime = -1
+	}
+
+	private trailMouse() {
+		if (!this.rgbTrail.value) {
+			this.trailHue = 0
+			this.trailPositions.clear()
+			return
+		}
+
+		this.trailHue = (this.trailHue + 1) % 360
+		this.trailPositions.push(InputManager.CursorOnScreen)
+
+		if (this.trailPositions.length > this.maxTrailLength) {
+			this.trailPositions.shift()
+		}
+
+		const trailLength = this.trailPositions.length,
+			lineColor = new Color(...MathSDK.HSVToRGB(this.trailHue, 1, 1, true, true))
+
+		for (let i = 1; i < trailLength; i++) {
+			const start = this.trailPositions[i - 1],
+				end = this.trailPositions[i],
+				alpha = (i / trailLength) * 255
+
+			const width = Math.max(
+				1,
+				// 10 * Math.exp(-3 * (trailLength - i) / trailLength),
+				12 * Math.pow(1 - (trailLength - i) / trailLength, 2)
+			)
+			// smoothing out gaps
+			const steps = Math.ceil(start.Distance(end) / 2)
+			for (let j = 0; j <= steps; j++) {
+				const lerpFactor = j / steps
+				const startPoint = start.Lerp(end, lerpFactor)
+				const endPoint = start.Lerp(end, (lerpFactor + 1 / steps) % 1)
+				RendererSDK.Line(startPoint, endPoint, lineColor.SetA(alpha), width)
+			}
+		}
 	}
 })()
