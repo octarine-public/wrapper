a6595e02978081d71bb889035fd8b8ed4fb51b35
SicdeX <sicdex@gmail.com>
2025-09-12 20:32:08 +0600

    add: update GameData

--- wrapper/Objects/Base/World.ts
+++ wrapper/Objects/Base/World.ts
@@ -1,4 +1,4 @@
-import { AttackSpeedData } from "../../Data/GameData"
+import { AttackSpeedData, Runes } from "../../Data/GameData"
 import { WrapperClass } from "../../Decorators"
 import { EventsSDK } from "../../Managers/EventsSDK"
 import { ConVarsSDK } from "../../Native/ConVarsSDK"
@@ -7,13 +7,21 @@ import { Entity } from "./Entity"
 @WrapperClass("CWorld")
 export class World extends Entity {}
 
-EventsSDK.on("PreEntityCreated", ent => {
-	if (!(ent instanceof World)) {
-		return
-	}
+function UpdateGameData() {
 	AttackSpeedData.SetMinMaxFactorInternal(
 		ConVarsSDK.GetFloat("dota_min_haste", 0.1),
 		ConVarsSDK.GetFloat("dota_max_haste", 7),
 		ConVarsSDK.GetFloat("dota_special_attack_delay", 0)
 	)
+	Runes.XPSpawnEverySeconds = ConVarsSDK.GetFloat(
+		"dota_xp_fountain_activation_interval",
+		Runes.XPSpawnEverySeconds
+	)
+	Runes.XPSpawnEveryMinutes = Runes.XPSpawnEverySeconds / 60
+}
+
+EventsSDK.on("PreEntityCreated", ent => {
+	if (ent instanceof World) {
+		UpdateGameData()
+	}
 })
