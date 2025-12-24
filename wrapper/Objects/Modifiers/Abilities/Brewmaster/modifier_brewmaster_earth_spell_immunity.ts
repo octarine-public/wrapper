e713d7db7d1bcc790ee1b6f55191dc7136967b16
SicdeX <sicdex@gmail.com>
2025-12-24 06:13:00 +0600

    fix: latest game update

--- wrapper/Objects/Modifiers/Abilities/Brewmaster/modifier_brewmaster_earth_spell_immunity.ts
+++ wrapper/Objects/Modifiers/Abilities/Brewmaster/modifier_brewmaster_earth_spell_immunity.ts
@@ -5,6 +5,8 @@ import { Modifier } from "../../../Base/Modifier"
 @WrapperClassModifier()
 export class modifier_brewmaster_earth_spell_immunity extends Modifier {
 	private cachedMres = 0
+	private slowResistance = 0
+	private cachedStatusResist = 0
 
 	protected readonly DeclaredFunction = new Map([
 		[
@@ -14,19 +16,33 @@ export class modifier_brewmaster_earth_spell_immunity extends Modifier {
 		[
 			EModifierfunction.MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS,
 			this.GetMagicalResistanceBonus.bind(this)
+		],
+		[
+			EModifierfunction.MODIFIER_PROPERTY_STATUS_RESISTANCE_STACKING,
+			this.GetStatusResistanceStacking.bind(this)
+		],
+		[
+			EModifierfunction.MODIFIER_PROPERTY_SLOW_RESISTANCE_STACKING,
+			this.GetSlowResistanceStacking.bind(this)
 		]
 	])
 	protected GetAbsoluteNoDamagePure(): [number, boolean] {
 		return [1, false]
 	}
+	protected GetSlowResistanceStacking(): [number, boolean] {
+		return [this.slowResistance, false]
+	}
 	protected GetMagicalResistanceBonus(params?: IModifierParams): [number, boolean] {
 		const ignoreSpellImmune = params?.IgnoreMagicResist ?? false
 		return !ignoreSpellImmune ? [this.cachedMres, false] : [0, false]
 	}
+	protected GetStatusResistanceStacking(): [number, boolean] {
+		return [this.cachedStatusResist, false]
+	}
 	protected UpdateSpecialValues(): void {
-		this.cachedMres = this.GetSpecialValue(
-			"magic_resist",
-			"brewmaster_earth_spell_immunity"
-		)
+		const name = "brewmaster_earth_spell_immunity"
+		this.cachedMres = this.GetSpecialValue("magic_resist", name)
+		this.slowResistance = this.GetSpecialValue("slow_resist", name)
+		this.cachedStatusResist = this.GetSpecialValue("status_resist", name)
 	}
 }
