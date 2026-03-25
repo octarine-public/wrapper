1ead760fe1290b7c1ea407c2f65ff9d12dd85c89
SicdeX <sicdex@gmail.com>
2026-03-25 22:03:14 +0600

    feat: add item_shawl and modifier_item_shawl
    - Add item_shawl class extending Item
    - Add modifier_item_shawl with magical resistance bonus property
    - Implement GetMagicalResistanceBonus using item_shawl special value
    - Update Imports.ts to export new item class

---
+++ wrapper/Objects/Items/item_shawl.ts
@@ -0,0 +1,5 @@
+import { WrapperClass } from "../../Decorators"
+import { Item } from "../Base/Item"
+
+@WrapperClass("item_shawl")
+export class item_shawl extends Item {}
---
+++ wrapper/Objects/Modifiers/Items/modifier_item_shawl.ts
@@ -0,0 +1,23 @@
+import { WrapperClassModifier } from "../../../Decorators"
+import { EModifierfunction } from "../../../Enums/EModifierfunction"
+import { Modifier } from "../../Base/Modifier"
+
+@WrapperClassModifier()
+export class modifier_item_shawl extends Modifier {
+	public readonly IsHidden = false
+	private cachedMres = 0
+
+	protected readonly DeclaredFunction = new Map([
+		[
+			EModifierfunction.MODIFIER_PROPERTY_MAGICAL_RESISTANCE_BONUS,
+			this.GetMagicalResistanceBonus.bind(this)
+		]
+	])
+	protected GetMagicalResistanceBonus(params?: IModifierParams): [number, boolean] {
+		const ignoreMagicResist = params?.IgnoreMagicResist ?? false
+		return !ignoreMagicResist ? [this.cachedMres, false] : [0, false]
+	}
+	protected UpdateSpecialValues() {
+		this.cachedMres = this.GetSpecialValue("bonus_magical_armor", "item_shawl")
+	}
+}
