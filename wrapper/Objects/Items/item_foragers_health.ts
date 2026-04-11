8b7876f50db538e64b6be36e3b7af3f6515dcdff
SicdeX <sicdex@gmail.com>
2026-04-11 16:33:42 +0600

    feat: implement Forager's Health item and its associated modifier

---
+++ wrapper/Objects/Items/item_foragers_health.ts
@@ -0,0 +1,5 @@
+import { WrapperClass } from "../../Decorators"
+import { Item } from "../Base/Item"
+
+@WrapperClass("item_foragers_health")
+export class item_foragers_health extends Item {}
--- wrapper/Objects/Modifiers/Items/modifier_item_foragers_health.ts
+++ wrapper/Objects/Modifiers/Items/modifier_item_foragers_health.ts
@@ -1,5 +1,22 @@
 import { WrapperClassModifier } from "../../../Decorators"
+import { EModifierfunction } from "../../../Enums/EModifierfunction"
 import { Modifier } from "../../Base/Modifier"
 
 @WrapperClassModifier()
-export class modifier_item_foragers_health extends Modifier {}
+export class modifier_item_foragers_health extends Modifier {
+	public readonly IsHidden = false
+	private cachedDamage = 0
+
+	protected readonly DeclaredFunction = new Map([
+		[
+			EModifierfunction.MODIFIER_PROPERTY_PREATTACK_BONUS_DAMAGE,
+			this.GetPreAttackBonusDamage.bind(this)
+		]
+	])
+	protected GetPreAttackBonusDamage(): [number, boolean] {
+		return [this.cachedDamage, false]
+	}
+	protected UpdateSpecialValues(): void {
+		this.cachedDamage = this.GetSpecialValue("bonus_damage", "item_foragers_health")
+	}
+}
