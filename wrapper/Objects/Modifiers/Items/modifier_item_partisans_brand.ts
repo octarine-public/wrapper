d5951dfdbb4df48269e0dafe941311c4a1430d3c
SicdeX <sicdex@gmail.com>
2026-04-17 20:17:49 +0600

    feat: implement modifier_item_partisans_brand with damage outgoing percentage property

--- wrapper/Objects/Modifiers/Items/modifier_item_partisans_brand.ts
+++ wrapper/Objects/Modifiers/Items/modifier_item_partisans_brand.ts
@@ -1,5 +1,35 @@
 import { WrapperClassModifier } from "../../../Decorators"
+import { EModifierfunction } from "../../../Enums/EModifierfunction"
+import { EntityManager } from "../../../Managers/EntityManager"
 import { Modifier } from "../../Base/Modifier"
 
-@WrapperClassModifier() // TODO: spell amp
-export class modifier_item_partisans_brand extends Modifier {}
+@WrapperClassModifier()
+export class modifier_item_partisans_brand extends Modifier {
+	private cachedOutgoingDamage = 0
+
+	protected readonly DeclaredFunction = new Map([
+		[
+			EModifierfunction.MODIFIER_PROPERTY_DAMAGEOUTGOING_PERCENTAGE,
+			this.GetDamageOutgoingPercentage.bind(this)
+		]
+	])
+	protected GetDamageOutgoingPercentage(params?: IModifierParams): [number, boolean] {
+		if (params === undefined) {
+			return [0, false]
+		}
+		const target = EntityManager.EntityByIndex(params.SourceIndex)
+		if (target === undefined || target === this.Parent) {
+			return [0, false]
+		}
+		if (!target.IsEnemy(this.Caster)) {
+			return [0, false]
+		}
+		return [this.cachedOutgoingDamage, false]
+	}
+	protected UpdateSpecialValues(): void {
+		this.cachedOutgoingDamage = this.GetSpecialValue(
+			"bonus_damage_pct",
+			"item_partisans_brand"
+		)
+	}
+}
